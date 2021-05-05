3f1173a7c4d0ade2a624f6583cc14e5c8d2b327a
김윤서 <yo26on@soongsil.ac.kr>
2021-05-05 11:34:13 +0900

    initial commit

---
+++ YDS-iOS/YDS_iOS.h
@@ -0,0 +1,18 @@
+//
+//  YDS_iOS.h
+//  YDS-iOS
+//
+//  Created by 김윤서 on 2021/05/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YDS_iOS.
+FOUNDATION_EXPORT double YDS_iOSVersionNumber;
+
+//! Project version string for YDS_iOS.
+FOUNDATION_EXPORT const unsigned char YDS_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YDS_iOS/PublicHeader.h>
+
+
