a95be54611f75c9593795cf9e6daff6addb19264
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2021-05-28 15:32:22 +0900

    Revert "Revert "Merge pull request #4 from yourssu/feature/YDS-1""
    This reverts commit ba2319870fc3d4474e97074eaff5b715bf74cd96.

---
+++ YDS-iOS/Atom/Label.swift
@@ -0,0 +1,47 @@
+//
+//  YDSLabel.swift
+//  YDS-iOS
+//
+//  Created by 김윤서 on 2021/05/05.
+//
+
+import UIKit
+
+public class Label: UILabel{
+    public var style : String.TypoStyle{
+        didSet{
+            setAttributedText()
+        }
+    }
+    
+    public init(style: String.TypoStyle) {
+        self.style = style
+        super.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override var text: String? {
+        didSet {
+            setAttributedText()
+        }
+    }
+
+    public override var textColor: UIColor!{
+        didSet{
+            setAttributedText()
+        }
+    }
+    
+    private func setAttributedText(){
+        if let text = self.text {
+            if let color = textColor {
+                self.attributedText = text.attributedString(byPreset: style, color: color)
+            } else {
+                self.attributedText = text.attributedString(byPreset: style)
+            }
+        }
+    }
+}
