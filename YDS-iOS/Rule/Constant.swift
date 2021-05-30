5c126f44b2a6a41d84dbb514fe9e7120ecb7e705
Gyuni <gyuni@soongsil.ac.kr>
2021-05-30 23:22:52 +0900

    constant로 border, rounding 정의

---
+++ YDS-iOS/Rule/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  YDS-iOS
+//
+//  Created by Gyuni on 2021/05/30.
+//
+
+import UIKit
+
+internal enum constant {
+    enum border {
+        static let thin: CGFloat = 1/UIScreen.main.scale
+        static let thick: CGFloat = 8
+    }
+    
+    enum rounding {
+        static let r2: CGFloat = 2
+        static let r4: CGFloat = 4
+        static let r8: CGFloat = 8
+    }
+}
