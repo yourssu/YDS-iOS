19cffd84f8c4c89332ab506a1ed7196122af923c
Gyuni <gyuni@soongsil.ac.kr>
2021-05-31 00:06:30 +0900

    열거형 네이밍 UpperCase로 변경

--- YDS-iOS/Rule/Constant.swift
+++ YDS-iOS/Rule/Constant.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-internal enum constant {
-    enum border {
+internal enum Constant {
+    enum Border {
         static let thin: CGFloat = 1/UIScreen.main.scale
         static let thick: CGFloat = 8
     }
     
-    enum rounding {
+    enum Rounding {
         static let r2: CGFloat = 2
         static let r4: CGFloat = 4
         static let r8: CGFloat = 8
