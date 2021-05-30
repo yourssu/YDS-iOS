c0d88132212666ec26093808b412be981eaa7b8f
Gyuni <gyuni@soongsil.ac.kr>
2021-05-31 00:20:24 +0900

    enum 접근자 public 으로 변경

--- YDS-iOS/Rule/Constant.swift
+++ YDS-iOS/Rule/Constant.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 public enum Constant {
-    enum Border {
+    public enum Border {
         static let thin: CGFloat = 1/UIScreen.main.scale
         static let thick: CGFloat = 8
     }
     
-    enum Rounding {
+    public enum Rounding {
         static let r2: CGFloat = 2
         static let r4: CGFloat = 4
         static let r8: CGFloat = 8
