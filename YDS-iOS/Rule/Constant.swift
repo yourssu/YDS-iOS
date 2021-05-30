51ed889320c051e06b08fc22790520b2a5fd59c9
Gyuni <gyuni@soongsil.ac.kr>
2021-05-31 01:53:52 +0900

    [Feature/YDS-10] constant로 border, rounding 정의 (#11)
    * constant로 border, rounding 정의
    
    * 열거형 네이밍 UpperCase로 변경
    
    * internal에서 public으로 변경
    
    * enum 접근자 public 으로 변경
    
    * 상수들도 public으로 명시

---
+++ YDS-iOS/Rule/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  YDS-iOS
+//
+//  Created by Gyuni on 2021/05/30.
+//
+
+import UIKit
+
+public enum Constant {
+    public enum Border {
+        public static let thin: CGFloat = 1/UIScreen.main.scale
+        public static let thick: CGFloat = 8
+    }
+    
+    public enum Rounding {
+        public static let r2: CGFloat = 2
+        public static let r4: CGFloat = 4
+        public static let r8: CGFloat = 8
+    }
+}
