091723c5d183690f519eb339eebb6fb30396ee01
Gyuni <gyuni@soongsil.ac.kr>
2021-06-11 14:58:56 +0900

    init 접근제한자 수정

--- YDS-iOS/Source/Atom/Toggle.swift
+++ YDS-iOS/Source/Atom/Toggle.swift
@@ -9,13 +9,17 @@ import UIKit
 
 public class Toggle: UISwitch {
 
-    public override init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
         self.onTintColor = YDSColor.buttonPoint
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func test() {
+        print("test")
     }
 
 }
