767b5691e1f5bbf0f6168dbab499a90a110810bc
yoonseo <yo26on@soongsil.ac.kr>
2021-06-15 14:31:02 +0900

    [YDS-16-2] Animation 정의

---
+++ YDS-iOS/Source/Rule/Animation.swift
@@ -0,0 +1,19 @@
+//
+//  Animation.swift
+//  YDS-iOS
+//
+//  Created by 김윤서 on 2021/06/15.
+//
+
+import UIKit
+
+internal enum Animation{
+    internal enum Duration{
+        internal static let s = 0.1
+        internal static let m = 0.25
+    }
+    
+    internal enum Options{
+        internal static let curveEaseInOut = UIView.AnimationOptions.curveEaseInOut
+    }
+}
