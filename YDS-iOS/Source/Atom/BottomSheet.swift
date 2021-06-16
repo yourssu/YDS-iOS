5f023a78917ef1e21ec953be7af6cb426d8d42ef
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2021-06-16 14:24:07 +0900

    [Feature/YDS-16] Bottom Sheet 정의 ,  YDS-Sample Scheme 추가 (#24)
    * [YDS-16] SnapKit,PanModal 추가
    
    * [YDS-16] YDS-Sample 모듈 추가
    
    * [YDS-16] BottomSheet PanModal 적용
    
    * [YDS-16] Bottom Sheet 정의
    
    * [YDS-16] 접근제한자 변경
    
    * [YDS-16-2] Animation,ScreenSize 파일 추가
    
    * [YDS-16-2] Animation 정의
    
    * [YDS-16-2] ScreenSize 정의
    
    * [YDS-16-2] Screen,Animation 사용하도록 변경
    
    * [YDS-16-2] 불필요한 부분 삭제
    
    * [YDS-16-2] Duration s  변경
    
    * [YDS-16-2] 네이밍 변경
    
    * [YDS-16] $0으로 변경
    
    * [YDS-16] BottomSheet 관련 숫자 상수로 정의

---
+++ YDS-iOS/Source/Atom/BottomSheet.swift
@@ -0,0 +1,118 @@
+//
+//  BottomSheet.swift
+//  YDS-iOS
+//
+//  Created by 김윤서 on 2021/06/11.
+//
+
+import PanModal
+import UIKit
+import SnapKit
+
+public class BottomSheet: UIViewController{
+    private let vStack: UIStackView = {
+        let vStack = UIStackView()
+        vStack.axis = .vertical
+        vStack.spacing = 0
+        vStack.alignment = .fill
+        return vStack
+    }()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setScrollView()
+    }
+    
+    public func addViews(views : [UIView]){
+        views.forEach {
+            vStack.addArrangedSubview($0)
+        }
+    }
+    
+    private func setScrollView(){
+        
+        let contentView = UIView()
+        
+        if let scrollView = panScrollable{
+            view.addSubview(scrollView)
+
+            scrollView.snp.makeConstraints {
+                $0.edges.equalToSuperview()
+            }
+            
+            scrollView.addSubview(contentView)
+            
+            contentView.snp.makeConstraints {
+                $0.edges.equalTo(0)
+                $0.width.equalTo(Screen.width)
+                $0.height.equalTo(Screen.height)
+            }
+                    
+            contentView.addSubview(vStack)
+            
+            vStack.snp.makeConstraints {
+                $0.top.equalTo(contentView.snp.top).offset(20)
+                $0.centerX.equalTo(contentView)
+            }
+        }
+    }
+}
+
+extension BottomSheet : PanModalPresentable{
+    public var panScrollable: UIScrollView? {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = YDSColor.bgNormal
+        scrollView.isScrollEnabled = false
+        return scrollView
+    }
+    
+    public var transitionDuration: Double{
+        return Animation.Duration.medium
+    }
+    
+    public var transitionAnimationOptions: UIView.AnimationOptions{
+        return Animation.Options.curveEaseInOut
+    }
+    
+    public var allowsDragToDismiss: Bool{
+        return true
+    }
+    
+    public var showDragIndicator: Bool{
+        return false
+    }
+    
+    public var cornerRadius: CGFloat{
+        return Constant.Rounding.r8
+    }
+    
+    public var panModalBackgroundColor: UIColor{
+        return YDSColor.dimNormal
+    }
+
+    public var longFormHeight: PanModalHeight {
+        let stackHeight = vStack.frame.height
+        vStack.layoutIfNeeded()
+        
+        var contentHeight : CGFloat = 0.0
+        let padding : CGFloat = 40.0
+        
+        if stackHeight + padding < BottomSheetHeight.min{
+            contentHeight = BottomSheetHeight.min
+        }
+        else if stackHeight > BottomSheetHeight.max{
+            contentHeight = BottomSheetHeight.max
+            panScrollable?.isScrollEnabled = true
+        }
+        else{
+            contentHeight = stackHeight + padding
+        }
+        
+        return .contentHeight(contentHeight)
+    }
+}
+
+fileprivate enum BottomSheetHeight{
+    static let min : CGFloat = 88.0
+    static let max : CGFloat = Screen.height - 88.0
+}
---
+++ YDS-iOS/Source/Rule/ScreenSize.swift
@@ -0,0 +1,13 @@
+//
+//  ScreenSize.swift
+//  YDS-iOS
+//
+//  Created by 김윤서 on 2021/06/15.
+//
+
+import UIKit
+
+public enum Screen{
+    public static let width = UIScreen.main.bounds.width
+    public static let height = UIScreen.main.bounds.height
+}
