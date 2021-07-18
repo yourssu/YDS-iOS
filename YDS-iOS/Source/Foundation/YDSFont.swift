185bbac7203c1251a00a6b932cd66bde48d084eb
Gyuni <gyuni@soongsil.ac.kr>
2021-07-18 18:48:11 +0900

    UIFont 선언을 var에서 let으로 변경

--- YDS-iOS/Source/Foundation/YDSFont.swift
+++ YDS-iOS/Source/Foundation/YDSFont.swift
@@ -8,23 +8,23 @@
 import UIKit
 
 public enum YDSFont {
-    public static var title1: UIFont { UIFont.systemFont(ofSize: 28, weight: .bold) }
-    public static var title2: UIFont { UIFont.systemFont(ofSize: 24, weight: .bold) }
-    public static var title3: UIFont { UIFont.systemFont(ofSize: 20, weight: .bold) }
+    public static let title1 = UIFont.systemFont(ofSize: 28, weight: .bold )
+    public static let title2 = UIFont.systemFont(ofSize: 24, weight: .bold )
+    public static let title3 = UIFont.systemFont(ofSize: 20, weight: .bold )
     
-    public static var subtitle1: UIFont { UIFont.systemFont(ofSize: 20, weight: .semibold) }
-    public static var subtitle2: UIFont { UIFont.systemFont(ofSize: 16, weight: .semibold) }
-    public static var subtitle3: UIFont { UIFont.systemFont(ofSize: 14, weight: .semibold) }
+    public static let subtitle1 = UIFont.systemFont(ofSize: 20, weight: .semibold )
+    public static let subtitle2 = UIFont.systemFont(ofSize: 16, weight: .semibold )
+    public static let subtitle3 = UIFont.systemFont(ofSize: 14, weight: .semibold )
     
-    public static var body1: UIFont { UIFont.systemFont(ofSize: 15, weight: .regular) }
-    public static var body2: UIFont { UIFont.systemFont(ofSize: 14, weight: .regular) }
+    public static let body1 = UIFont.systemFont(ofSize: 15, weight: .regular )
+    public static let body2 = UIFont.systemFont(ofSize: 14, weight: .regular )
     
-    public static var button0: UIFont { UIFont.systemFont(ofSize: 16, weight: .medium) }
-    public static var button1: UIFont { UIFont.systemFont(ofSize: 16, weight: .semibold) }
-    public static var button2: UIFont { UIFont.systemFont(ofSize: 14, weight: .semibold) }
-    public static var button3: UIFont { UIFont.systemFont(ofSize: 14, weight: .regular) }
-    public static var button4: UIFont { UIFont.systemFont(ofSize: 12, weight: .medium) }
+    public static let button0 = UIFont.systemFont(ofSize: 16, weight: .medium )
+    public static let button1 = UIFont.systemFont(ofSize: 16, weight: .semibold )
+    public static let button2 = UIFont.systemFont(ofSize: 14, weight: .semibold )
+    public static let button3 = UIFont.systemFont(ofSize: 14, weight: .regular )
+    public static let button4 = UIFont.systemFont(ofSize: 12, weight: .medium )
     
-    public static var caption1: UIFont { UIFont.systemFont(ofSize: 12, weight: .regular) }
-    public static var caption2: UIFont { UIFont.systemFont(ofSize: 11, weight: .regular) }
+    public static let caption1 = UIFont.systemFont(ofSize: 12, weight: .regular )
+    public static let caption2 = UIFont.systemFont(ofSize: 11, weight: .regular )
 }
