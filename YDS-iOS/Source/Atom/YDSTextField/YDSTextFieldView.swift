6926b0090376b3ee9efdc6c5c737aad3c213b838
Gyuni <gyuni@soongsil.ac.kr>
2021-07-26 23:54:21 +0900

    [Feature/YDS-34] TextField 4종 구현

---
+++ YDS-iOS/Source/Atom/YDSTextField/YDSTextFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  YDSTextField.swift
+//  YDS-iOS
+//
+//  Created by Gyuni on 2021/07/26.
+//
+
+import UIKit
+
+internal struct YDSTextFieldView {
+    internal enum Dimension {
+        //  subviewSpacing: CGFloat
+        //  fieldLabel, textField, helperLabel 사이 간격입니다.
+        static let subviewSpacing: CGFloat = 8
+        
+        //  helperLabelHorizontalMargin: CGFloat
+        //  helperLabel의 좌우 마진값입니다.
+        static let helperLabelHorizontalMargin: CGFloat = 8
+    }
+}
