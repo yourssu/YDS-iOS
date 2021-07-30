509669de5138b9a1fe22ac2ca50b125b86f6301b
Gyuni <gyuni@soongsil.ac.kr>
2021-07-31 02:56:44 +0900

    [YDS-37] UIButton의 setBackgroundColor가 UIGraphicsImageRender를 사용하도록 수정

--- YDS-iOS/Source/Atom/YDSBoxButton.swift
+++ YDS-iOS/Source/Atom/YDSBoxButton.swift
@@ -435,14 +435,17 @@ public class YDSBoxButton: UIButton {
 
 extension UIButton {
     internal func setBackgroundColor(_ color: UIColor?, for state: UIControl.State) {
-        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-        context.setFillColor(color?.cgColor ?? UIColor.clear.cgColor)
-        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
+        let render = UIGraphicsImageRenderer(size: CGSize(width: 1.0, height: 1.0))
         
-        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-         
-        self.setBackgroundImage(backgroundImage, for: state)
+        let image = render.image { context in
+            if let color = color {
+                color.setFill()
+            } else {
+                UIColor.clear.setFill()
+            }
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        }
+        
+        self.setBackgroundImage(image, for: state)
     }
 }
