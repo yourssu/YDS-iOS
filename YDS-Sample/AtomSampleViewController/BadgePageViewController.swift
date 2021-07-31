85d140bb61942f0305bb3fc6b85bfbc2cd996839
Gyuni <gyuni@soongsil.ac.kr>
2021-08-01 00:02:03 +0900

    [#44] Badge 샘플 페이지 뷰 구현

--- YDS-Sample/AtomSampleViewController/BadgePageViewController.swift
+++ YDS-Sample/AtomSampleViewController/BadgePageViewController.swift
@@ -6,24 +6,69 @@
 //
 
 import UIKit
+import YDS_iOS
+import SnapKit
 
-class BadgePageViewController: UIViewController {
+class BadgePageViewController: StoryBookViewController {
+        
+    let sampleBadge: YDSBadge = {
+        let badge = YDSBadge()
+        return badge
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
+        addOptions()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        setViewProperty()
+        setViewHierarchy()
+        setAutolayout()
     }
-    */
+    
+    private func setViewProperty() {
+        self.title = "Badge"
+        
+        //  mono 컬러의 badge가 배경이랑 색이 겹쳐서
+        //  이 페이지는 임의로 하얀 컬러로 설정했습니다.
+        sampleView.backgroundColor = YDSColor.bgNormal
+    }
+    
+    private func setViewHierarchy() {
+        sampleView.addSubview(sampleBadge)
+        
+    }
+    
+    private func setAutolayout() {
+        sampleBadge.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+    
+    private func addOptions() {
+        addOption(description: "text",
+                  defaultValue: "광고") {
+            self.sampleBadge.text = $0
+        }
+        
+        addOption(description: "icon",
+                  images: ydsIconArray,
+                  defaultImage: YDSIcon.adbadgeFilled) {
+            self.sampleBadge.icon = $0
+        }
+        
+        addOption(description: "color",
+                  cases: YDSItemColor.allCases,
+                  defaultIndex: 3) {
+            self.sampleBadge.color = $0
+        }
+        
+    }
 
+}
+
+extension YDSItemColor: CaseIterable {
+    public static var allCases: [YDSItemColor] = [.mono, .green, .emerald, .aqua, .blue, .indigo, .violet, .purple, .pink]
 }
