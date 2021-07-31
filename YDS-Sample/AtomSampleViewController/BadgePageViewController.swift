9616533b1f520176e45179ea6dbec2a5a0718b0e
Gyuni <gyuni@soongsil.ac.kr>
2021-08-01 00:01:47 +0900

    [#44] Badge 구현

---
+++ YDS-Sample/AtomSampleViewController/BadgePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BadgePageViewController.swift
+//  YDS-Sample
+//
+//  Created by Gyuni on 2021/07/31.
+//
+
+import UIKit
+
+class BadgePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
