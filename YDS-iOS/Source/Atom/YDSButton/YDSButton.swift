87e8c15fbeca5ce594a4b3049b563671f113496b
Gyuni <gyuni@soongsil.ac.kr>
2021-07-31 18:59:02 +0900

    [#46] YDSButton Protocol 작성

---
+++ YDS-iOS/Source/Atom/YDSButton/YDSButton.swift
@@ -0,0 +1,16 @@
+//
+//  YDSButton.swift
+//  YDS-iOS
+//
+//  Created by Gyuni on 2021/07/31.
+//
+
+import UIKit
+
+internal protocol YDSButton {
+    var isDisabled: Bool { get set }
+    var isWarned: Bool { get set }
+    var text: String? { get set }
+    var leftIcon: UIImage? { get set }
+    var rightIcon: UIImage? { get set }
+}
