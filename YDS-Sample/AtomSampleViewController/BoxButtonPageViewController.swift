88362f38d01f06617ab21cc38a0f2c5516a13054
Gyuni <gyuni@soongsil.ac.kr>
2021-07-31 19:08:39 +0900

    [#46] 원활한 sample앱 테스트를 위해 ydsIconArray 추가

--- YDS-Sample/AtomSampleViewController/BoxButtonPageViewController.swift
+++ YDS-Sample/AtomSampleViewController/BoxButtonPageViewController.swift
@@ -50,13 +50,13 @@ class BoxButtonPageViewController: StoryBookViewController {
         }
         
         addOption(description: "leftIcon",
-                  images: [YDSIcon.playcircleFilled, YDSIcon.groundFilled],
+                  images: ydsIconArray,
                   defaultIndex: nil) {
             self.sampleButton.leftIcon = $0
         }
         
         addOption(description: "rightIcon",
-                  images: [YDSIcon.playcircleFilled, YDSIcon.groundFilled],
+                  images: ydsIconArray,
                   defaultIndex: nil) {
             self.sampleButton.rightIcon = $0
         }
