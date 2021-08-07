9e4c76668e08c282f36b74747928dfe7ad1d512d
Gyuni <gyuni@soongsil.ac.kr>
2021-08-07 23:23:20 +0900

    [#54] BottomBar 기본 세팅

---
+++ YDS-Sample/ComponentSampleViewController/BottomBarControllerPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BottomBarViewController.swift
+//  YDS-Sample
+//
+//  Created by Gyuni on 2021/08/07.
+//
+
+import UIKit
+
+class BottomBarControllerPageViewController: YDSBottomBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
