523494b5ab592c5ac78364f3bea21b687e973507
Gyuni <gyuni@soongsil.ac.kr>
2021-08-09 02:10:02 +0900

    [#54] Animation, Feedback 추가

--- YDS-iOS/Source/Component/YDSBottomBarController.swift
+++ YDS-iOS/Source/Component/YDSBottomBarController.swift
@@ -9,19 +9,20 @@ import UIKit
 
 open class YDSBottomBarController: UITabBarController {
     
-    private enum Dimension {
-        static let iconSize = 24
+    open override var viewControllers: [UIViewController]? {
+        didSet { setTabBarItemImageInsets() }
     }
     
-    private var verticalPadding: CGFloat {
-        get {
-            (tabBar.bounds.height - CGFloat(Dimension.iconSize))/2
-        }
+    private enum Dimension {
+        static let itemImageVerticalInset: CGFloat = 6
     }
+        
+    let feedbackGenerator = UIImpactFeedbackGenerator(style: .light)
     
     open override func viewDidLoad() {
         super.viewDidLoad()
-
+        feedbackGenerator.prepare()
+        
         setupView()
     }
     
@@ -35,5 +36,42 @@ open class YDSBottomBarController: UITabBarController {
         tabBar.backgroundColor = YDSColor.bgElevated
         tabBar.isTranslucent = false
     }
+    
+    public func setTabBarItemImageInsets() {
+        viewControllers?.forEach {
+            if $0.tabBarItem.title == nil {
+                $0.tabBarItem.imageInsets = UIEdgeInsets(top: Dimension.itemImageVerticalInset,
+                                                         left: 0,
+                                                         bottom: -Dimension.itemImageVerticalInset,
+                                                         right: 0)
+            }
+        }
+    }
 
+}
+
+extension YDSBottomBarController {
+    
+    private enum Transform {
+        static let length: TimeInterval = Animation.Duration.medium
+        static let scale: CGFloat = 1.2
+        static let dumpingRatio: CGFloat = 0.5
+    }
+    
+    open override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        feedbackGenerator.impactOccurred()
+        feedbackGenerator.prepare()
+                
+        guard let itemView = item.value(forKey: "view") as? UIView else { return }
+        
+        let propertyAnimator = UIViewPropertyAnimator(duration: Transform.length,
+                                                      dampingRatio: 0.5) {
+            itemView.transform = CGAffineTransform.identity.scaledBy(x: Transform.scale, y: Transform.scale)
+        }
+        propertyAnimator.addAnimations({ itemView.transform = .identity },
+                                       delayFactor: CGFloat(Transform.length))
+        propertyAnimator.startAnimation()
+    }
+    
 }
