cbc0c7f1b507cdf514502d1a25e0d430c7b2c4b2
Gyuni <gyuni@soongsil.ac.kr>
2021-08-12 14:17:45 +0900

    [#53] 주석 추가

--- YDS-iOS/Source/Component/TopBar/YDSNavigationController.swift
+++ YDS-iOS/Source/Component/TopBar/YDSNavigationController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 public class YDSNavigationController: UINavigationController {
-        
+    
+    //  MARK: - 외부에서 지정할 수 있는 속성
+    
+    /**
+     RootViewController의 NavigationBar 상단에 들어가는
+     굵은 Title의 String입니다.
+     */
     public override var title: String? {
         didSet { self.titleLabel.text = title }
     }
@@ -19,6 +25,13 @@ public class YDSNavigationController: UINavigationController {
         return label
     }()
     
+    /**
+     YDS 스타일을 적용한 NavigationController를 생성합니다.
+     
+     - Parameters:
+        - title: RootViewController의 NavigationBar 상단에 들어가는 굵은 title의 String 값입니다.
+        - rootViewController: NavigationController의 rootViewController입니다.
+     */
     public init(title: String?, rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
         self.title = title
@@ -35,23 +48,35 @@ public class YDSNavigationController: UINavigationController {
         setupFullWidthBackGesture()
     }
     
+    /**
+     뷰를 세팅합니다.
+     */
     private func setupView() {
         setProperties()
     }
     
+    /**
+     각종 프로퍼티를 세팅합니다.
+     */
     private func setProperties() {
-        setBarAppearance()
+        setNavigationBarBackIndicator()
         setNavigationBarProperties()
     }
     
 
-    private func setBarAppearance() {
+    /**
+     NavigationBar의 뒤로가기 아이콘 이미지를 설정합니다.
+     */
+    private func setNavigationBarBackIndicator() {
         let barAppearance =
             UINavigationBar.appearance(whenContainedInInstancesOf: [YDSNavigationController.self])
         barAppearance.backIndicatorImage = YDSIcon.arrowLeftLine
         barAppearance.backIndicatorTransitionMaskImage = YDSIcon.arrowLeftLine
     }
     
+    /**
+     NavigationBar의 프로퍼티를 설정합니다.
+     */
     private func setNavigationBarProperties() {
         navigationBar.tintColor = YDSColor.buttonNormal
         navigationBar.isTranslucent = false
@@ -62,11 +87,22 @@ public class YDSNavigationController: UINavigationController {
         ]
     }
     
+    /**
+     화면 중 어디를 스와이프해도 ViewController를 Pop 하기 위해
+     UIPanGestureRecognizer를 생성합니다.
+     */
     private lazy var fullWidthBackGestureRecognizer = UIPanGestureRecognizer()
 }
 
 
 extension YDSNavigationController {
+    
+    /**
+     ViewController가 Push될 때
+     count가 1이라면(=root밖에 없다면) 굵은 titleLabel을 세팅합니다.
+     count가 1이 아니라면(=root 이외에 다른게 있다면) backBarButtonItem을 초기화하여
+     backButton 옆에 상위 viewController의 title이 나타나지 않도록 합니다.
+     */
     public override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         super.pushViewController(viewController, animated: true)
         if viewControllers.count == 1 {
@@ -89,6 +125,10 @@ extension YDSNavigationController {
 
 extension YDSNavigationController: UIGestureRecognizerDelegate {
     
+    /**
+     화면 중 어디를 스와이프해도 ViewController를 Pop 하기 위해
+     UIPanGestureRecognizer를 설정합니다.
+     */
     private func setupFullWidthBackGesture() {
         guard
             let interactivePopGestureRecognizer = interactivePopGestureRecognizer,
@@ -102,6 +142,12 @@ extension YDSNavigationController: UIGestureRecognizerDelegate {
         view.addGestureRecognizer(fullWidthBackGestureRecognizer)
     }
     
+    /**
+     UINavigationController의
+     기본 SwipeBack 옵션이 True 이면서
+     Stack에 ViewController가 1개 이상일 때(=root 이외에 다른 ViewController가 있을 때)
+     PanGestureRecognizer에 의해 SwipeBack 액션을 실행합니다.
+     */
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         let isSystemSwipeToBackEnabled = interactivePopGestureRecognizer?.isEnabled == true
         let isThereStackedViewControllers = viewControllers.count > 1
