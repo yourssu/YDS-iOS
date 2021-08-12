a9a22cb5d94c4820c135bc3b930d3c35987abd8c
Gyuni <gyuni@soongsil.ac.kr>
2021-08-12 14:17:45 +0900

    [#53] 주석 추가

--- YDS-iOS/Source/Component/TopBar/YDSTopBarButton.swift
+++ YDS-iOS/Source/Component/TopBar/YDSTopBarButton.swift
@@ -9,6 +9,11 @@ import UIKit
 
 public class YDSTopBarButton: UIButton {
 
+    //  MARK: - 외부에서 지정할 수 있는 속성
+    
+    /**
+     버튼을 비활성화 시킬 때 사용합니다.
+     */
     public var isDisabled: Bool = false {
         didSet {
             self.isEnabled = !isDisabled
@@ -16,9 +21,10 @@ public class YDSTopBarButton: UIButton {
         }
     }
     
-    //  isHighlighted: Bool?
-    //  기본 속성을 override한 후 didSet을 설정하여
-    //  값이 바뀔 때 ( = 버튼을 누르거나 땠을 때 ) 그에 맞춰 색을 바꿔줍니다.
+    /**
+     기본 속성을 override한 후 didSet을 설정하여
+     값이 바뀔 때 ( = 버튼을 누르거나 땠을 때 ) 그에 맞춰 색을 바꿔줍니다.
+     */
     public override var isHighlighted: Bool {
         didSet {
             if oldValue != isHighlighted {
@@ -29,14 +35,22 @@ public class YDSTopBarButton: UIButton {
     
     //  MARK: - 내부에서 사용되는 변수
 
-    //  fgColor: UIColor?
-    //  버튼의 아이콘, 글자 컬러입니다.
+    /**
+     버튼의 아이콘, 글자 컬러입니다.
+     */
     private var fgColor: UIColor?
     
-    //  fgPressedColor: UIColor?
-    //  버튼이 pressed 되었을 때 아이콘, 글자 컬러입니다.
+    /**
+     버튼이 pressed 되었을 때 아이콘, 글자 컬러입니다.
+     */
     private var fgPressedColor: UIColor?
     
+    
+    //  MARK: - 내부에서 사용되는 상수
+    
+    /**
+     내부에서 사용되는 각종 레이아웃 관련 수치입니다.
+     */
     private enum Dimension {
         enum content {
             static let imageSize: CGFloat = 24
@@ -46,12 +60,26 @@ public class YDSTopBarButton: UIButton {
         }
     }
     
+    //  MARK: - 메소드
+    
+    /**
+     텍스트만 들어가는 버튼을 생성합니다. 이 버튼은 TopBar에만 사용하길 권장합니다.
+     
+     - Parameters:
+        - text: 버튼에 들어갈 글귀입니다.
+     */
     public init(text: String?) {
         super.init(frame: .zero)
         self.setTitle(text, for: .normal)
         setupView()
     }
     
+    /**
+     이미지만 들어가는 버튼을 생성합니다. 이 버튼은 TopBar에만 사용하길 권장합니다.
+     
+     - Parameters:
+        - image: 버튼에 들어갈 이미지입니다.
+     */
     public init(image: UIImage?) {
         super.init(frame: .zero)
         self.setImage(image?
@@ -65,11 +93,17 @@ public class YDSTopBarButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /**
+     뷰를 세팅합니다.
+     */
     private func setupView() {
         setProperties()
         setColor()
     }
     
+    /**
+     각종 프로퍼티를 세팅합니다.
+     */
     private func setProperties() {
         self.adjustsImageWhenHighlighted = false
         self.adjustsImageWhenDisabled = false
@@ -81,6 +115,9 @@ public class YDSTopBarButton: UIButton {
                                               right: Dimension.content.Padding.horizontal)
     }
     
+    /**
+     버튼의 컬러를 세팅합니다.
+     */
     private func setColor() {
         if isDisabled {
             fgColor = YDSColor.buttonDisabled
@@ -96,8 +133,10 @@ public class YDSTopBarButton: UIButton {
         setTintColorBasedOnIsHighlighted()
     }
     
-    //  setTintColorBasedOnIsHighlighted()
-    //  isHighlighted 값에 맞추어 tintColor를 변경합니다.
+    /**
+     setTintColorBasedOnIsHighlighted()
+     isHighlighted 값에 맞추어 tintColor를 변경합니다.
+     */
     private func setTintColorBasedOnIsHighlighted() {
         self.tintColor = !isHighlighted
             ? fgColor
