6396b2de49c4395b243527587aa767d696e70bb3
Gyuni <gyuni@soongsil.ac.kr>
2021-08-22 14:18:50 +0900

    [#56] Pods 관련 파일 삭제

--- Pods/Target Support Files/Pods-YDS-iOS-YDS-Sample/Pods-YDS-iOS-YDS-Sample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YDS_iOS_YDS_Sample : NSObject
-@end
-@implementation PodsDummy_Pods_YDS_iOS_YDS_Sample
-@end
--- Pods/Target Support Files/Pods-YDS-iOS/Pods-YDS-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YDS_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_YDS_iOS
-@end
