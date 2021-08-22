fe8ec30f5fdbd042f45c206d7708455c714cc342
Gyuni <gyuni@soongsil.ac.kr>
2021-08-22 21:43:23 +0900

    [#55] Color Theme 구현

---
+++ YDS/Source/Foundation/YDSThemeColor.swift
@@ -0,0 +1,17 @@
+//
+//  YDSThemeColor.swift
+//  YDS
+//
+//  Created by Gyuni on 2021/08/22.
+//
+
+import Foundation
+
+public class YDSThemeColor {
+    public enum Theme {
+        case ground
+        case soomsil
+    }
+    
+    public static var theme: Theme = .soomsil
+}
