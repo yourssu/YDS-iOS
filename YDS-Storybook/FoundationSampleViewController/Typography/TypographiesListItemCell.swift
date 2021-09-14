0a7841cb166ac11cfb177f981243ab8bdd55bb0c
Gyuni <gyuni@soongsil.ac.kr>
2021-09-14 14:17:53 +0900

    [#78] TypoStyle 관리 방법 변경

---
+++ YDS-Storybook/FoundationSampleViewController/Typography/TypographiesListItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  TypographiesListItemCell.swift
+//  YDS-Storybook
+//
+//  Created by Gyuni on 2021/09/14.
+//
+
+import UIKit
+
+class TypographiesListItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
