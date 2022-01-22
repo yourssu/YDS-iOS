88103c7b2975d8ccafb8321244601612d9165dd9
Yonghyun Kim <lygon55555@gmail.com>
2022-01-23 02:08:03 +0900

    [#102] BottomSheetViewController 추가

---
+++ YDS-Storybook/AtomSampleViewController/BottomSheetViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BottomSheetViewController.swift
+//  YDS-Storybook
+//
+//  Created by Yonghyun on 2022/01/23.
+//
+
+import SnapKit
+import UIKit
+import YDS
+
+class BottomSheetViewController: StoryBookViewController {
+    
+    let sampleBottomSheet: YDSBottomSheet = {
+        let bottomSheet = YDSBottomSheet()
+        return bottomSheet
+    }()
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
