04c3d383eb54080ff67cb6e8bc2e197c0bd135b1
Yonghyun Kim <lygon55555@gmail.com>
2022-03-13 21:28:57 +0900

    [#111] init 삭제

--- YDS-Storybook/FoundationSampleViewController/Icon/IconsPageViewController.swift
+++ YDS-Storybook/FoundationSampleViewController/Icon/IconsPageViewController.swift
@@ -12,16 +12,7 @@ import SnapKit
 class IconsPageViewController: UIViewController {
     
     private let tableViewArea = UIView()
-    private let iconsListTableViewController: IconsListTableViewController
-    
-    init() {
-        iconsListTableViewController = IconsListTableViewController()
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let iconsListTableViewController = IconsListTableViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
