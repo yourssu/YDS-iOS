1314d0771a01d1149d2235323ae40cc502744228
kati-kms <qkrguswndk@naver.com>
2022-04-02 16:57:35 +0900

    [#122] Scene Delegate 생성

--- YDS-Storybook/AppDelegate.swift
+++ YDS-Storybook/AppDelegate.swift
@@ -11,18 +11,17 @@ import YDS
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let navigationController = YDSNavigationController(title: "Storybook", rootViewController: PageListViewController())
-        
-        window?.rootViewController = navigationController
-        
-        window?.makeKeyAndVisible()
-        
         return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 
 }
