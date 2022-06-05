0da5c21413554d2c6e838a3d6f48ef687a3b50dd
Yonghyun <lygon55555@gmail.com>
2022-06-06 01:18:49 +0900

    [#110] 접근제어자 수정

--- YDS/Source/Component/TabBar/Classes/YDSTabBarViewController.swift
+++ YDS/Source/Component/TabBar/Classes/YDSTabBarViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 /// `select(pagingItem:animated:)` to set the initial view controller.
 /// You can also use the same method to programmatically navigate to
 /// other view controllers.
-open class YDSTabBarViewController:
+public class YDSTabBarViewController:
     UIViewController,
     UICollectionViewDelegate,
     PageViewControllerDataSource,
@@ -25,33 +25,33 @@ open class YDSTabBarViewController:
 
     /// The size for each of the menu items. _Default:
     /// .sizeToFit(minWidth: 150, height: 40)_
-    public var menuItemSize: PagingMenuItemSize {
+    private var menuItemSize: PagingMenuItemSize {
         get { return options.menuItemSize }
         set { options.menuItemSize = newValue }
     }
 
     /// Determine the spacing between the menu items. _Default: 0_
-    public var menuItemSpacing: CGFloat {
+    private var menuItemSpacing: CGFloat {
         get { return options.menuItemSpacing }
         set { options.menuItemSpacing = newValue }
     }
 
     /// Determine the horizontal constraints of menu item label. _Default: 20_
-    public var menuItemLabelSpacing: CGFloat {
+    private var menuItemLabelSpacing: CGFloat {
         get { return options.menuItemLabelSpacing }
         set { options.menuItemLabelSpacing = newValue }
     }
 
     /// Determine the insets at around all the menu items. _Default:
     /// UIEdgeInsets.zero_
-    public var menuInsets: UIEdgeInsets {
+    private var menuInsets: UIEdgeInsets {
         get { return options.menuInsets }
         set { options.menuInsets = newValue }
     }
 
     /// Determine how users can interact with the menu items.
     /// _Default: .scrolling_
-    public var menuInteraction: PagingMenuInteraction {
+    private var menuInteraction: PagingMenuInteraction {
         get { return options.menuInteraction }
         set { options.menuInteraction = newValue }
     }
@@ -61,7 +61,7 @@ open class YDSTabBarViewController:
     /// property will initialize the new layout type and update the
     /// collection view.
     /// _Default: PagingCollectionViewLayout.self_
-    public var menuLayoutClass: PagingCollectionViewLayout.Type {
+    private var menuLayoutClass: PagingCollectionViewLayout.Type {
         get { return options.menuLayoutClass }
         set { options.menuLayoutClass = newValue }
     }
@@ -69,7 +69,7 @@ open class YDSTabBarViewController:
     /// Add an indicator view to the selected menu item. The indicator
     /// width will be equal to the selected menu items width. Insets
     /// only apply horizontally. _Default: .visible_
-    public var indicatorOptions: PagingIndicatorOptions {
+    private var indicatorOptions: PagingIndicatorOptions {
         get { return options.indicatorOptions }
         set { options.indicatorOptions = newValue }
     }
@@ -77,62 +77,62 @@ open class YDSTabBarViewController:
     /// The class type for the indicator view. Override this if you want
     /// your use your own subclass of PagingIndicatorView. _Default:
     /// PagingIndicatorView.self_
-    public var indicatorClass: PagingIndicatorView.Type {
+    private var indicatorClass: PagingIndicatorView.Type {
         get { return options.indicatorClass }
         set { options.indicatorClass = newValue }
     }
 
     /// Determine the color of the indicator view.
-    public var indicatorColor: UIColor {
+    private var indicatorColor: UIColor {
         get { return options.indicatorColor }
         set { options.indicatorColor = newValue }
     }
 
     /// Updates the content inset for the menu items based on the
     /// .safeAreaInsets property. _Default: true_
-    public var includeSafeAreaInsets: Bool {
+    private var includeSafeAreaInsets: Bool {
         get { return options.includeSafeAreaInsets }
         set { options.includeSafeAreaInsets = newValue }
     }
 
     /// The font used for title label on the menu items.
-    public var font: UIFont {
+    private var font: UIFont {
         get { return options.font }
         set { options.font = newValue }
     }
 
     /// The font used for the currently selected menu item.
-    public var selectedFont: UIFont {
+    private var selectedFont: UIFont {
         get { return options.selectedFont }
         set { options.selectedFont = newValue }
     }
 
     /// The color of the title label on the menu items.
-    public var textColor: UIColor {
+    private var textColor: UIColor {
         get { return options.textColor }
         set { options.textColor = newValue }
     }
 
     /// The text color for the currently selected menu item.
-    public var selectedTextColor: UIColor {
+    private var selectedTextColor: UIColor {
         get { return options.selectedTextColor }
         set { options.selectedTextColor = newValue }
     }
 
     /// The background color for the menu items.
-    public var backgroundColor: UIColor {
+    private var backgroundColor: UIColor {
         get { return options.backgroundColor }
         set { options.backgroundColor = newValue }
     }
 
     /// The background color for the selected menu item.
-    public var selectedBackgroundColor: UIColor {
+    private var selectedBackgroundColor: UIColor {
         get { return options.selectedBackgroundColor }
         set { options.selectedBackgroundColor = newValue }
     }
 
     /// The background color for the view behind the menu items.
-    public var menuBackgroundColor: UIColor {
+    private var menuBackgroundColor: UIColor {
         get { return options.menuBackgroundColor }
         set { options.menuBackgroundColor = newValue }
     }
@@ -140,14 +140,14 @@ open class YDSTabBarViewController:
     /// The current state of the menu items. Indicates whether an item
     /// is currently selected or is scrolling to another item. Can be
     /// used to get the distance and progress of any ongoing transition.
-    public var state: PagingState {
+    private var state: PagingState {
         return pagingController.state
     }
 
     /// The `PagingItem`'s that are currently visible in the collection
     /// view. The items in this array are not necessarily the same as
     /// the `visibleCells` property on `UICollectionView`.
-    public var visibleItems: PagingItems {
+    private var visibleItems: PagingItems {
         return pagingController.visibleItems
     }
 
@@ -158,7 +158,7 @@ open class YDSTabBarViewController:
     /// Use this property when you have a fixed amount of view
     /// controllers. If you need to support infinitely large data
     /// sources, use the infiniteDataSource property instead.
-    public weak var dataSource: PagingViewControllerDataSource? {
+    private weak var dataSource: PagingViewControllerDataSource? {
         didSet {
             configureDataSource()
         }
@@ -169,17 +169,17 @@ open class YDSTabBarViewController:
     /// before or after a given `PagingItem`. The `PagingItem` protocol
     /// is used to generate menu items for all the view controllers,
     /// without having to actually allocate them before they are needed.
-    public weak var infiniteDataSource: PagingViewControllerInfiniteDataSource?
+    private weak var infiniteDataSource: PagingViewControllerInfiniteDataSource?
 
     /// Use this delegate to get notified when the user is scrolling or
     /// when an item is selected.
-    public weak var delegate: PagingViewControllerDelegate?
+    private weak var delegate: PagingViewControllerDelegate?
 
     /// Use this delegate if you want to manually control the width of
     /// your menu items. Self-sizing cells is not supported at the
     /// moment, so you have to use this if you have a custom cell that
     /// you want to size based on its content.
-    public weak var sizeDelegate: PagingViewControllerSizeDelegate? {
+    private weak var sizeDelegate: PagingViewControllerSizeDelegate? {
         didSet {
             pagingController.sizeDelegate = self
         }
@@ -190,20 +190,20 @@ open class YDSTabBarViewController:
     /// setting the customization properties on `PagingViewController`.
     /// You can also use your own subclass of the layout by defining the
     /// `menuLayoutClass` property.
-    public private(set) var collectionViewLayout: PagingCollectionViewLayout
+    private(set) var collectionViewLayout: PagingCollectionViewLayout
 
     /// Used to display the menu items that scrolls along with the
     /// content. Using a collection view means you can create custom
     /// cells that display pretty much anything. By default, scrolling
     /// is enabled in the collection view.
-    public let collectionView: UICollectionView
+    private let collectionView: UICollectionView
 
     /// Used to display the view controllers that you are paging between.
-    public let pageViewController: PageViewController
+    private let pageViewController: PageViewController
 
     /// An instance that stores all the customization so that it's
     /// easier to share between other classes.
-    public private(set) var options: PagingOptions {
+    private(set) var options: PagingOptions {
         didSet {
             if options.menuLayoutClass != oldValue.menuLayoutClass {
                 let layout = createLayout(layout: options.menuLayoutClass.self)
@@ -279,7 +279,7 @@ open class YDSTabBarViewController:
 
     /// Reload the data for the menu items. This method will not reload
     /// the view controllers.
-    open func reloadMenu() {
+    private func reloadMenu() {
         var updatedItems: [PagingItem] = []
 
         switch dataSourceReference {
@@ -309,7 +309,7 @@ open class YDSTabBarViewController:
     /// will not work when using PagingViewControllerInfiniteDataSource
     /// as we then need to know what the initial item should be. You
     /// should use the reloadData(around:) method in that case.
-    open func reloadData() {
+    private func reloadData() {
         var updatedItems: [PagingItem] = []
 
         switch dataSourceReference {
@@ -342,7 +342,7 @@ open class YDSTabBarViewController:
     ///
     /// - Parameter pagingItem: The `PagingItem` that will be selected
     /// after the data reloads.
-    open func reloadData(around pagingItem: PagingItem) {
+    private func reloadData(around pagingItem: PagingItem) {
         switch dataSourceReference {
         case let .static(dataSource):
             dataSource.reloadItems()
@@ -363,7 +363,7 @@ open class YDSTabBarViewController:
     /// - Parameter pagingItem: The `PagingItem` to be displayed.
     /// - Parameter animated: A boolean value that indicates whether
     /// the transtion should be animated. Default is false.
-    open func select(pagingItem: PagingItem, animated: Bool = false) {
+    private func select(pagingItem: PagingItem, animated: Bool = false) {
         pagingController.select(pagingItem: pagingItem, animated: animated)
     }
 
@@ -373,7 +373,7 @@ open class YDSTabBarViewController:
     /// - Parameter index: The index of the `PagingItem` to be displayed.
     /// - Parameter animated: A boolean value that indicates whether
     /// the transtion should be animated. Default is false.
-    open func select(index: Int, animated: Bool = false) {
+    private func select(index: Int, animated: Bool = false) {
         switch dataSourceReference {
         case let .static(dataSource):
             let pagingItem = dataSource.items[index]
@@ -386,7 +386,7 @@ open class YDSTabBarViewController:
         }
     }
 
-    open override func loadView() {
+    public override func loadView() {
         view = PagingView(
             options: options,
             collectionView: collectionView,
@@ -396,7 +396,7 @@ open class YDSTabBarViewController:
         view.backgroundColor = .systemBackground
     }
 
-    open override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         addChild(pageViewController)
@@ -408,7 +408,7 @@ open class YDSTabBarViewController:
         configureContentInteraction()
     }
 
-    open override func viewDidLayoutSubviews() {
+    public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
         // We need generate the menu items when the view appears for the
@@ -420,7 +420,7 @@ open class YDSTabBarViewController:
         }
     }
 
-    open override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    public override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         coordinator.animate(alongsideTransition: { _ in
             self.pagingController.transitionSize()
@@ -430,14 +430,14 @@ open class YDSTabBarViewController:
     /// Register cell class for paging cell
     /// - Parameter cellClass: paging cell's class
     /// - Parameter pagingItemType: paging item type for specifying cell identifier
-    public func register(_ cellClass: AnyClass?, for pagingItemType: PagingItem.Type) {
+    private func register(_ cellClass: AnyClass?, for pagingItemType: PagingItem.Type) {
         collectionView.register(cellClass, forCellWithReuseIdentifier: String(describing: pagingItemType))
     }
 
     /// Register nib for paging cell
     /// - Parameter nib: paging cell's nib
     /// - Parameter pagingItemType: paging item type for specifying cell identifier
-    public func register(_ nib: UINib?, for pagingItemType: PagingItem.Type) {
+    private func register(_ nib: UINib?, for pagingItemType: PagingItem.Type) {
         collectionView.register(nib, forCellWithReuseIdentifier: String(describing: pagingItemType))
     }
 
@@ -494,69 +494,69 @@ open class YDSTabBarViewController:
 
     // MARK: UIScrollViewDelegate
 
-    open func scrollViewDidScroll(_: UIScrollView) {
+    public func scrollViewDidScroll(_: UIScrollView) {
         pagingController.menuScrolled()
     }
 
-    open func scrollViewWillBeginDragging(_: UIScrollView) {
+    public func scrollViewWillBeginDragging(_: UIScrollView) {
         return
     }
 
-    open func scrollViewWillEndDragging(_: UIScrollView, withVelocity _: CGPoint, targetContentOffset _: UnsafeMutablePointer<CGPoint>) {
+    public func scrollViewWillEndDragging(_: UIScrollView, withVelocity _: CGPoint, targetContentOffset _: UnsafeMutablePointer<CGPoint>) {
         return
     }
 
-    open func scrollViewDidEndDragging(_: UIScrollView, willDecelerate _: Bool) {
+    public func scrollViewDidEndDragging(_: UIScrollView, willDecelerate _: Bool) {
         return
     }
 
-    open func scrollViewDidEndScrollingAnimation(_: UIScrollView) {
+    public func scrollViewDidEndScrollingAnimation(_: UIScrollView) {
         return
     }
 
-    open func scrollViewWillBeginDecelerating(_: UIScrollView) {
+    public func scrollViewWillBeginDecelerating(_: UIScrollView) {
         return
     }
 
-    open func scrollViewDidEndDecelerating(_: UIScrollView) {
+    public func scrollViewDidEndDecelerating(_: UIScrollView) {
         return
     }
 
     // MARK: UICollectionViewDelegate
 
-    open func collectionView(_: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    public func collectionView(_: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let pagingItem = pagingController.visibleItems.pagingItem(for: indexPath)
         delegate?.pagingViewController(self, didSelectItem: pagingItem)
         pagingController.select(indexPath: indexPath, animated: true)
     }
 
-    open func collectionView(_: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
+    public func collectionView(_: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
         return proposedContentOffset
     }
 
-    open func collectionView(_: UICollectionView, didUnhighlightItemAt _: IndexPath) {
+    public func collectionView(_: UICollectionView, didUnhighlightItemAt _: IndexPath) {
         return
     }
 
-    open func collectionView(_: UICollectionView, didHighlightItemAt _: IndexPath) {
+    public func collectionView(_: UICollectionView, didHighlightItemAt _: IndexPath) {
         return
     }
 
-    open func collectionView(_: UICollectionView, didDeselectItemAt _: IndexPath) {
+    public func collectionView(_: UICollectionView, didDeselectItemAt _: IndexPath) {
         return
     }
 
-    open func collectionView(_: UICollectionView, willDisplay _: UICollectionViewCell, forItemAt _: IndexPath) {
+    public func collectionView(_: UICollectionView, willDisplay _: UICollectionViewCell, forItemAt _: IndexPath) {
         return
     }
 
-    open func collectionView(_: UICollectionView, didEndDisplaying _: UICollectionViewCell, forItemAt _: IndexPath) {
+    public func collectionView(_: UICollectionView, didEndDisplaying _: UICollectionViewCell, forItemAt _: IndexPath) {
         return
     }
 
     // MARK: PageViewControllerDataSource
 
-    open func pageViewController(_: PageViewController, viewControllerBeforeViewController _: UIViewController) -> UIViewController? {
+    public func pageViewController(_: PageViewController, viewControllerBeforeViewController _: UIViewController) -> UIViewController? {
         guard
             let dataSource = infiniteDataSource,
             let currentPagingItem = state.currentPagingItem,
@@ -565,7 +565,7 @@ open class YDSTabBarViewController:
         return dataSource.pagingViewController(self, viewControllerFor: pagingItem)
     }
 
-    open func pageViewController(_: PageViewController, viewControllerAfterViewController _: UIViewController) -> UIViewController? {
+    public func pageViewController(_: PageViewController, viewControllerAfterViewController _: UIViewController) -> UIViewController? {
         guard
             let dataSource = infiniteDataSource,
             let currentPagingItem = state.currentPagingItem,
@@ -576,7 +576,7 @@ open class YDSTabBarViewController:
 
     // MARK: PageViewControllerDelegate
 
-    open func pageViewController(_: PageViewController, isScrollingFrom startingViewController: UIViewController, destinationViewController: UIViewController?, progress: CGFloat) {
+    public func pageViewController(_: PageViewController, isScrollingFrom startingViewController: UIViewController, destinationViewController: UIViewController?, progress: CGFloat) {
         guard let currentPagingItem = state.currentPagingItem else { return }
 
         pagingController.contentScrolled(progress: progress)
@@ -590,7 +590,7 @@ open class YDSTabBarViewController:
         )
     }
 
-    open func pageViewController(_: PageViewController, willStartScrollingFrom startingViewController: UIViewController, destinationViewController: UIViewController) {
+    public func pageViewController(_: PageViewController, willStartScrollingFrom startingViewController: UIViewController, destinationViewController: UIViewController) {
         if let upcomingPagingItem = state.upcomingPagingItem {
             delegate?.pagingViewController(
                 self,
@@ -601,7 +601,7 @@ open class YDSTabBarViewController:
         }
     }
 
-    open func pageViewController(_: PageViewController, didFinishScrollingFrom startingViewController: UIViewController, destinationViewController: UIViewController, transitionSuccessful: Bool) {
+    public func pageViewController(_: PageViewController, didFinishScrollingFrom startingViewController: UIViewController, destinationViewController: UIViewController, transitionSuccessful: Bool) {
         if transitionSuccessful {
             pagingController.contentFinishedScrolling()
         }
