1d059df836745e3ab92a47a522d6b0561eef90d9
Yonghyun <lygon55555@gmail.com>
2022-07-10 00:45:38 +0900

    [#141] SPM 수정

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     dependencies: [
         .package(name: "PanModal", url: "https://github.com/slackhq/PanModal.git", from: "1.0.0"),
         .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit", from: "5.0.0"),
+        .package(name: "Parchment", url: "https://github.com/yourssu/Parchment", from: "3.7.2"),
     ],
 
     targets: [
@@ -25,6 +26,7 @@ let package = Package(
             dependencies: [
                 .product(name: "PanModal", package: "PanModal"),
                 .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "Parchment", package: "Parchment"),
             ],
             path: "YDS/Source",
             resources: [.process("YDS/Source/Foundation/YDSIcon.xcassets")]
