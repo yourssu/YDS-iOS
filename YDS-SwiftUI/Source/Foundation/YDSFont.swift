42614d9d9efcc090d6a8eb877a2204cf1cb3b975
chongin12 <chongin12@naver.com>
2023-07-27 01:52:44 +0900

    [#189] YDSFont 추가

---
+++ YDS-SwiftUI/Source/Foundation/YDSFont.swift
@@ -0,0 +1,34 @@
+//
+//  YDSFont.swift
+//  YDS-SwiftUI
+//
+//  Created by 정종인 on 2023/07/27.
+//
+
+import SwiftUI
+
+public enum YDSFont {
+    public static let display1 = Font.system(size: 40, weight: .bold)
+    public static let display2 = Font.system(size: 32, weight: .bold )
+
+    public static let title1 = Font.system(size: 28, weight: .bold )
+    public static let title2 = Font.system(size: 24, weight: .bold )
+    public static let title3 = Font.system(size: 20, weight: .bold )
+
+    public static let subtitle1 = Font.system(size: 20, weight: .semibold )
+    public static let subtitle2 = Font.system(size: 16, weight: .semibold )
+    public static let subtitle3 = Font.system(size: 14, weight: .semibold )
+
+    public static let body1 = Font.system(size: 15, weight: .regular )
+    public static let body2 = Font.system(size: 14, weight: .regular )
+
+    public static let button0 = Font.system(size: 16, weight: .medium )
+    public static let button1 = Font.system(size: 16, weight: .semibold )
+    public static let button2 = Font.system(size: 14, weight: .semibold )
+    public static let button3 = Font.system(size: 14, weight: .regular )
+    public static let button4 = Font.system(size: 12, weight: .medium )
+
+    public static let caption0 = Font.system(size: 12, weight: .semibold )
+    public static let caption1 = Font.system(size: 12, weight: .regular )
+    public static let caption2 = Font.system(size: 11, weight: .regular )
+}
