c19bfd5016b9e23c55a2b503ca0694da6439fc53
chongin12 <chongin12@naver.com>
2023-07-30 23:00:00 +0900

    [#189] Foundation 요소 추가

---
+++ YDS-SwiftUI/Source/Foundation/YDSIcon.swift
@@ -0,0 +1,106 @@
+//
+//  YDSIcon.swift
+//  YDS-iOS
+//
+//  Created by 정종인 on 2023/07/27.
+//
+
+import SwiftUI
+
+public enum YDSIcon {
+    public static var adbadgeFilled: Image { .load(name: "ic_adbadge_filled") }
+    public static var adbadgeLine: Image { .load(name: "ic_adbadge_line") }
+    public static var arrowDownLine: Image { .load(name: "ic_arrow_down_line") }
+    public static var arrowLeftLine: Image { .load(name: "ic_arrow_left_line") }
+    public static var arrowRightLine: Image { .load(name: "ic_arrow_right_line") }
+    public static var arrowUpLine: Image { .load(name: "ic_arrow_up_line") }
+    public static var bellFilled: Image { .load(name: "ic_bell_filled") }
+    public static var bellLine: Image { .load(name: "ic_bell_line") }
+    public static var bellmuteLine: Image { .load(name: "ic_bellmute_line") }
+    public static var boardFilled: Image { .load(name: "ic_board_filled") }
+    public static var boardLine: Image { .load(name: "ic_board_line") }
+    public static var bookFilled: Image { .load(name: "ic_book_filled") }
+    public static var bookLine: Image { .load(name: "ic_book_line") }
+    public static var calendarFilled: Image { .load(name: "ic_calendar_filled") }
+    public static var calendarLine: Image { .load(name: "ic_calendar_line") }
+    public static var cameraFilled: Image { .load(name: "ic_camera_filled") }
+    public static var cameraLine: Image { .load(name: "ic_camera_line") }
+    public static var cameracircleLine: Image { .load(name: "ic_cameracircle_line") }
+    public static var checkLine: Image { .load(name: "ic_check_line") }
+    public static var checkcircleFilled: Image { .load(name: "ic_checkcircle_filled") }
+    public static var checkcircleLine: Image { .load(name: "ic_checkcircle_line") }
+    public static var clipLine: Image { .load(name: "ic_clip_line") }
+    public static var commentFilled: Image { .load(name: "ic_comment_filled") }
+    public static var commentLine: Image { .load(name: "ic_comment_line") }
+    public static var dotbadgeLine: Image { .load(name: "ic_dotbadge_line") }
+    public static var dotsHorizontalLine: Image { .load(name: "ic_dots_horizontal_line") }
+    public static var dotsVerticalLine: Image { .load(name: "ic_dots_vertical_line") }
+    public static var emojiaddLine: Image { .load(name: "ic_emojiadd_line")}
+    public static var eyeclosedLine: Image { .load(name: "ic_eyeclosed_line")}
+    public static var eyeopenLine: Image { .load(name: "ic_eyeopen_line")}
+    public static var foodFilled: Image { .load(name: "ic_food_filled") }
+    public static var foodLine: Image { .load(name: "ic_food_line") }
+    public static var foodcalendarFilled: Image { .load(name: "ic_foodcalendar_filled") }
+    public static var foodcalendarLine: Image { .load(name: "ic_foodcalendar_line") }
+    public static var groundFilled: Image { .load(name: "ic_ground_filled") }
+    public static var groundLine: Image { .load(name: "ic_ground_line") }
+    public static var heartFilled: Image { .load(name: "ic_heart_filled") }
+    public static var heartLine: Image { .load(name: "ic_heart_line") }
+    public static var homeFilled: Image { .load(name: "ic_home_filled") }
+    public static var homeLine: Image { .load(name: "ic_home_line") }
+    public static var listLine: Image { .load(name: "ic_list_line") }
+    public static var lockFilled: Image { .load(name: "ic_lock_filled") }
+    public static var lockLine: Image { .load(name: "ic_lock_line") }
+    public static var newFilled: Image { .load(name: "ic_new_filled") }
+    public static var newLine: Image { .load(name: "ic_new_line") }
+    public static var noticeFilled: Image { .load(name: "ic_notice_filled") }
+    public static var noticeLine: Image { .load(name: "ic_notice_line") }
+    public static var penFilled: Image { .load(name: "ic_pen_filled") }
+    public static var penLine: Image { .load(name: "ic_pen_line") }
+    public static var personFilled: Image { .load(name: "ic_person_filled") }
+    public static var personLine: Image { .load(name: "ic_person_line") }
+    public static var personcircleLine: Image { .load(name: "ic_personcircle_line") }
+    public static var pictureFilled: Image { .load(name: "ic_picture_filled") }
+    public static var pictureLine: Image { .load(name: "ic_picture_line") }
+    public static var pinFilled: Image { .load(name: "ic_pin_filled") }
+    public static var pinLine: Image { .load(name: "ic_pin_line") }
+    public static var playcircleFilled: Image { .load(name: "ic_playcircle_filled") }
+    public static var playcircleLine: Image { .load(name: "ic_playcircle_line") }
+    public static var plusLine: Image { .load(name: "ic_plus_line") }
+    public static var rankFilled: Image { .load(name: "ic_rank_filled") }
+    public static var rankLine: Image { .load(name: "ic_rank_line") }
+    public static var recommentLine: Image { .load(name: "ic_recomment_line") }
+    public static var refreshLine: Image { .load(name: "ic_refresh_line") }
+    public static var savecircleFilled: Image { .load(name: "ic_savecircle_filled") }
+    public static var savecircleLine: Image { .load(name: "ic_savecircle_line") }
+    public static var schoolcalendarFilled: Image { .load(name: "ic_schoolcalendar_filled") }
+    public static var schoolcalendarLine: Image { .load(name: "ic_schoolcalendar_line") }
+    public static var searchLine: Image { .load(name: "ic_search_line") }
+    public static var sharecircleFilled: Image { .load(name: "ic_sharecircle_filled") }
+    public static var sharecircleLine: Image { .load(name: "ic_sharecircle_line") }
+    public static var starFilled: Image { .load(name: "ic_star_filled") }
+    public static var starLine: Image { .load(name: "ic_star_line") }
+    public static var thumbDownFilled: Image { .load(name: "ic_thumb_down_filled") }
+    public static var thumbDownLine: Image { .load(name: "ic_thumb_down_line") }
+    public static var thumbUpFilled: Image { .load(name: "ic_thumb_up_filled") }
+    public static var thumbUpLine: Image { .load(name: "ic_thumb_up_line") }
+    public static var timecalendarFilled: Image { .load(name: "ic_timecalendar_filled") }
+    public static var timecalendarLine: Image { .load(name: "ic_timecalendar_line") }
+    public static var trashcanFilled: Image { .load(name: "ic_trashcan_filled") }
+    public static var trashcanLine: Image { .load(name: "ic_trashcan_line") }
+    public static var warningcircleFilled: Image { .load(name: "ic_warningcircle_filled") }
+    public static var warningcircleLine: Image { .load(name: "ic_warningcircle_line") }
+    public static var xLine: Image { .load(name: "ic_x_line") }
+    public static var xcircleFilled: Image { .load(name: "ic_xcircle_filled") }
+}
+
+extension Image {
+    fileprivate static func load(name: String) -> Image {
+#if SWIFT_PACKAGE
+        let image = Image(name, bundle: .module)
+#else
+        let image = Image(name, bundle: YDSBundle.bundle)
+#endif
+        return image
+    }
+}
---
+++ YDS-SwiftUI/Source/Foundation/YDSSemanticColor.swift
@@ -0,0 +1,352 @@
+//
+//  SemanticColor.swift
+//  YDS-iOS
+//
+//  Created by 정종인 on 2023/07/27.
+//
+import SwiftUI
+
+public enum YDSColor {
+    private static func color(light: Color, dark: Color? = nil) -> Color {
+        if let dark = dark {
+            if UITraitCollection.current.userInterfaceStyle == .dark {
+                return dark
+            } else {
+                return light
+            }
+        } else {
+            return light
+        }
+    }
+
+    // MARK: - Background
+
+    public static var bgNormal: Color {
+        return color(light: .white000, dark: .black000)
+    }
+
+    public static var bgElevated: Color {
+        return color(light: .white000, dark: .black000)
+    }
+
+    public static var bgRecomment: Color {
+        return color(light: .gray050, dark: .realBlack)
+    }
+
+    public static var bgPressed: Color {
+        return color(light: .black000A10, dark: .white000A5)
+    }
+
+    public static var bgSelected: Color {
+        return color(light: .black000A10, dark: .white000A5)
+    }
+
+    public static var bgNormalDark: Color {
+        return color(light: .realBlack, dark: .realBlack)
+    }
+
+    public static var bgElevatedDark: Color {
+        return color(light: .realBlack, dark: .realBlack)
+    }
+
+    public static var bgDimDark: Color {
+        return color(light: .gray900A30, dark: .black000A30)
+    }
+
+    // MARK: - Text
+
+    public static var textPrimary: Color {
+        return color(light: .black000, dark: .gray900)
+    }
+
+    public static var textSecondary: Color {
+        return color(light: .gray900, dark: .gray800)
+    }
+
+    public static var textTertiary: Color {
+        return color(light: .gray600, dark: .gray600)
+    }
+
+    public static var textDisabled: Color {
+        return color(light: .gray500, dark: .gray400)
+    }
+
+    public static var textBright: Color {
+        return color(light: .white000, dark: .white000)
+    }
+
+    public static var textPointed: Color {
+        return color(light: .pointColor400, dark: .pointColor400)
+    }
+
+    public static var textWarned: Color {
+        return color(light: .warningRed400, dark: .warningRed400)
+    }
+
+    // MARK: - Dim
+
+    public static var dimNormal: Color {
+        return color(light: .gray900A30, dark: .black000A30)
+    }
+
+    public static var dimThick: Color {
+        return color(light: .gray900A70, dark: .black000A70)
+    }
+
+    public static var dimThickBright: Color {
+        return color(light: .white000A70, dark: .white000A70)
+    }
+
+    // MARK: - Border
+
+    public static var borderThin: Color {
+        return color(light: .gray100, dark: .gray100)
+    }
+
+    public static var borderNormal: Color {
+        return color(light: .black000A10, dark: .white000A10)
+    }
+
+    public static var borderThick: Color {
+        return color(light: .gray500, dark: .gray500)
+    }
+
+    // MARK: - Button
+
+    public static var buttonNormal: Color {
+        return color(light: .gray700, dark: .gray700)
+    }
+
+    public static var buttonNormalPressed: Color {
+        return color(light: .gray600, dark: .gray600)
+    }
+
+    public static var buttonBG: Color {
+        return color(light: .gray200, dark: .gray200)
+    }
+
+    public static var buttonEmojiBG: Color {
+        return color(light: .gray100, dark: .gray100)
+    }
+
+    public static var buttonBright: Color {
+        return color(light: .white000, dark: .white000)
+    }
+
+    public static var buttonDisabled: Color {
+        return color(light: .gray500, dark: .gray500)
+    }
+
+    public static var buttonDisabledBG: Color {
+        return color(light: .gray200, dark: .gray200)
+    }
+
+    public static var buttonPoint: Color {
+        return color(light: .pointColor400, dark: .pointColor400)
+    }
+
+    public static var buttonPointPressed: Color {
+        return color(light: .pointColor300, dark: .pointColor300)
+    }
+
+    public static var buttonPointBG: Color {
+        return color(light: .pointColor050, dark: .pointColor050)
+    }
+
+    public static var buttonWarned: Color {
+        return color(light: .warningRed400, dark: .warningRed400)
+    }
+
+    public static var buttonWarnedPressed: Color {
+        return color(light: .warningRed300, dark: .warningRed300)
+    }
+
+    public static var buttonWarnedBG: Color {
+        return color(light: .warningRed050, dark: .warningRed050)
+    }
+
+    // MARK: - BottomBar
+
+    public static var bottomBarNormal: Color {
+        return color(light: .gray600, dark: .gray600)
+    }
+
+    public static var bottomBarSelected: Color {
+        return color(light: .gray800, dark: .gray800)
+    }
+
+    // MARK: - InputField
+
+    public static var inputFieldNormal: Color {
+        return color(light: .white000, dark: .black000)
+    }
+
+    public static var inputFieldElevated: Color {
+        return color(light: .gray100, dark: .gray100)
+    }
+
+    // MARK: - Toast
+
+    public static var toastBG: Color {
+        return color(light: .gray800, dark: .gray300)
+    }
+
+    // MARK: - Tooltip
+
+    public static var tooltipBG: Color {
+        return color(light: .gray700, dark: .gray400)
+    }
+
+    public static var tooltipPoint: Color {
+        return color(light: .pointColor400, dark: .pointColor400)
+    }
+
+    // MARK: - Picker
+
+    public static var dimPickerReverse: Color {
+        return color(light: .white000A70, dark: .white000A70)
+    }
+
+    // MARK: - Pressed
+
+    public static var pressed: Color {
+        return color(light: .black000A10, dark: .white000A10)
+    }
+
+    // MARK: - Shadow
+    //  다크모드에서는 Shadow를 사용하지 않습니다.
+
+    public static var shadowThin: Color {
+        return color(light: .gray400)
+    }
+
+    public static var shadowNormal: Color {
+        return color(light: .gray500)
+    }
+
+    // MARK: - MonoItem
+
+    public static var monoItemPrimary: Color {
+        return color(light: .gray700, dark: .gray700)
+    }
+
+    public static var monoItemBG: Color {
+        return color(light: .gray100, dark: .gray100)
+    }
+
+    public static var monoItemText: Color {
+        return color(light: .gray800, dark: .gray800)
+    }
+
+    // MARK: - GreenItem
+
+    public static var greenItemPrimary: Color {
+        return color(light: .green300, dark: .green300)
+    }
+
+    public static var greenItemBG: Color {
+        return color(light: .green050, dark: .green050)
+    }
+
+    public static var greenItemText: Color {
+        return color(light: .green800, dark: .green800)
+    }
+
+    // MARK: - EmeraldItem
+
+    public static var emeraldItemPrimary: Color {
+        return color(light: .emerald300, dark: .emerald300)
+    }
+
+    public static var emeraldItemBG: Color {
+        return color(light: .emerald050, dark: .emerald050)
+    }
+
+    public static var emeraldItemText: Color {
+        return color(light: .emerald800, dark: .emerald800)
+    }
+
+    // MARK: - AquaItem
+
+    public static var aquaItemPrimary: Color {
+        return color(light: .aqua300, dark: .aqua300)
+    }
+
+    public static var aquaItemBG: Color {
+        return color(light: .aqua050, dark: .aqua050)
+    }
+
+    public static var aquaItemText: Color {
+        return color(light: .aqua700, dark: .aqua700)
+    }
+
+    // MARK: - BlueItem
+
+    public static var blueItemPrimary: Color {
+        return color(light: .blue300, dark: .blue300)
+    }
+
+    public static var blueItemBG: Color {
+        return color(light: .blue050, dark: .blue050)
+    }
+
+    public static var blueItemText: Color {
+        return color(light: .blue700, dark: .blue700)
+    }
+
+    // MARK: - IndigoItem
+
+    public static var indigoItemPrimary: Color {
+        return color(light: .indigo300, dark: .indigo300)
+    }
+
+    public static var indigoItemBG: Color {
+        return color(light: .indigo050, dark: .indigo050)
+    }
+
+    public static var indigoItemText: Color {
+        return color(light: .indigo400, dark: .indigo400)
+    }
+
+    // MARK: - VioletItem
+
+    public static var violetItemPrimary: Color {
+        return color(light: .violet300, dark: .violet300)
+    }
+
+    public static var violetItemBG: Color {
+        return color(light: .violet050, dark: .violet050)
+    }
+
+    public static var violetItemText: Color {
+        return color(light: .violet400, dark: .violet400)
+    }
+
+    // MARK: - PurpleItem
+
+    public static var purpleItemPrimary: Color {
+        return color(light: .purple300, dark: .purple300)
+    }
+
+    public static var purpleItemBG: Color {
+        return color(light: .purple050, dark: .purple050)
+    }
+
+    public static var purpleItemText: Color {
+        return color(light: .purple400, dark: .purple400)
+    }
+
+    // MARK: - PinkItem
+
+    public static var pinkItemPrimary: Color {
+        return color(light: .pink300, dark: .pink300)
+    }
+
+    public static var pinkItemBG: Color {
+        return color(light: .pink050, dark: .pink050)
+    }
+
+    public static var pinkItemText: Color {
+        return color(light: .pink600, dark: .pink600)
+    }
+}
