c0324b5dff1b06ce6a0f0e152fa9e7e6b8fd8a2f
chongin12 <chongin12@naver.com>
2023-08-17 04:48:06 +0900

    [#189] Exported 설정

---
+++ YDS/Source/Exported.swift
@@ -0,0 +1,10 @@
+//
+//  Exported.swift
+//  YDS
+//
+//  Created by 정종인 on 2023/08/17.
+//
+
+import Foundation
+@_exported import YDS_Essential
+// YDS만 import해도 YDS_Essential 안에 있는 요소에 접근이 가능하도록 설정.
