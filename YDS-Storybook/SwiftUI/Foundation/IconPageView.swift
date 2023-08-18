37430224add39f1ae3cd32bf2d037513f349cb4c
김규철 <rbcjf122@gmail.com>
2023-08-18 17:17:32 +0900

    [#194] IconPageView 파일 추가

---
+++ YDS-Storybook/SwiftUI/Foundation/IconPageView.swift
@@ -0,0 +1,20 @@
+//
+//  IconPageView.swift
+//  YDS-Storybook
+//
+//  Created by 김규철 on 2023/08/18.
+//
+
+import SwiftUI
+
+struct IconPageView: View {
+    var body: some View {
+        Text("화이팅화이팅화이팅")
+    }
+}
+
+struct IconPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        IconPageView()
+    }
+}
