092a7c99b2398dd63704b1fb054489dd6ae5c22d
Sanghyeon Sim <5338095@gmail.com>
2023-08-19 18:47:44 +0900

    [#199] Storybook 프로토콜 및 Option enum 추가

--- YDS-Storybook/SwiftUI/Storybook/Storybook.swift
+++ YDS-Storybook/SwiftUI/Storybook/Storybook.swift
@@ -5,4 +5,17 @@
 //  Created by 정지혁 on 2023/08/19.
 //
 
-import Foundation
+import SwiftUI
+
+protocol Storybook {
+    associatedtype Content: View
+    
+    var title: String { get }
+    var sampleView: Content { get }
+    var divider: Content { get }
+    var options: [Option] { get }
+}
+
+enum Option {
+    
+}
