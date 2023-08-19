66ef39aa18ec1dab27d11dbeef37c79528ccfca5
jayden000106 <cars0106@icloud.com>
2023-08-19 18:18:32 +0900

    [#199] Storybook 파일 추가
    - Storybook 및 OptionView 구현에 필요한 파일 추가

---
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/OptionalImageOptionView.swift
@@ -0,0 +1,20 @@
+//
+//  OptionalImageOptionView.swift
+//  YDS-Storybook
+//
+//  Created by 정지혁 on 2023/08/19.
+//
+
+import SwiftUI
+
+struct OptionalImageOptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OptionalImageOptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionalImageOptionView()
+    }
+}
---
+++ YDS-Storybook/SwiftUI/Storybook/Storybook.swift
@@ -0,0 +1,8 @@
+//
+//  Storybook.swift
+//  YDS-Storybook
+//
+//  Created by 정지혁 on 2023/08/19.
+//
+
+import Foundation
