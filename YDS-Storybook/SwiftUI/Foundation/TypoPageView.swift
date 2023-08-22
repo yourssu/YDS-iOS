43b2b4a7128a4af83da279f108cb150124c16f84
Bibi-urssu <bibi.urssu@gmail.com>
2023-08-23 05:42:35 +0900

    [#202] 코드 리뷰 반영

--- YDS-Storybook/SwiftUI/Foundation/TypoPageView.swift
+++ YDS-Storybook/SwiftUI/Foundation/TypoPageView.swift
@@ -5,7 +5,6 @@
 //  Created by 박지윤 on 2023/08/22.
 //
 // swiftlint:disable nesting
-// swiftlint:disable line_length
 
 import SwiftUI
 import YDS_SwiftUI
@@ -14,84 +13,77 @@ struct TypoListItem: View {
     private enum Dimension {
         enum Spacing {
             static let vstack: CGFloat = 16
-            static let horizontal: CGFloat = 20
+            static let hstack: CGFloat = 30
+            static let textSpacing: CGFloat = 9
             static let listItem: CGFloat = 2
         }
 
-        enum Description {
-            enum Padding {
-                static let leading: CGFloat = -150
-                static let trailing: CGFloat = -55
-            }
+        enum Padding {
+            static let id: CGFloat = 10
+            static let hstack: CGFloat = 15
+        }
+
+        enum Rectangle {
+            static let idCornerRadius: CGFloat = 2
+            static let idHeight: CGFloat = 24
+            static let infoCornerRadius: CGFloat = 8
+            static let infoHeight: CGFloat = 95
         }
     }
     let fontInfo: WrappedSwiftUITypo
     var body: some View {
-        HStack {
-            VStack(spacing: 16) {
-                VStack(alignment: .leading, spacing: Dimension.Spacing.vstack) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 2)
-                            .fill(YDSColor.monoItemBG)
-                            .frame(height: 24)
-                        HStack {
-                            Text(self.fontInfo.id)
-                                .font(YDSFont.caption1)
-                                .foregroundColor(YDSColor.monoItemText)
-                                .padding(.leading, 10)
-                            Spacer()
-                        }
-                    }
-                    Text("계절이 지나가는 하늘에는 가을로 가득 차 있습니다.\n나는 아무 걱정도 없이 가을 속의 별들을 다 헬 듯합니다.\n가슴 속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은 쉬이 아침이 오는 까닭이요, 내일 밤이 남은 까닭이요, 아직 나의 청춘이 다하지 않은 까닭입니다.")
-                        .font(self.fontInfo.font ?? .title2)
-                        .foregroundColor(YDSColor.textPrimary)
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 8)
-                            .fill(YDSColor.monoItemBG)
-                            .frame(height: 95)
-                        VStack(spacing: 7) {
-                            HStack {
-                                Text("size")
-                                    .font(YDSFont.subtitle3)
-                                    .foregroundColor(YDSColor.textTertiary)
-                                    .padding(.horizontal, Dimension.Description.Padding.leading)
-                                Text(self.fontInfo.size)
-                                    .font(YDSFont.body2)
-                                    .foregroundColor(YDSColor.monoItemText)
-                                    .padding(.horizontal, Dimension.Description.Padding.trailing)
-                            }
-                            HStack {
-                                Text("weight")
-                                    .font(YDSFont.subtitle3)
-                                    .foregroundColor(YDSColor.textTertiary)
-                                    .padding(.horizontal, Dimension.Description.Padding.leading)
-                                Text(self.fontInfo.weight)
-                                    .font(YDSFont.body2)
-                                    .foregroundColor(YDSColor.monoItemText)
-                                    .padding(.horizontal, Dimension.Description.Padding.trailing)
-                            }
-                            HStack {
-                                Text("lineHeight")
-                                    .font(YDSFont.subtitle3)
-                                    .foregroundColor(YDSColor.textTertiary)
-                                    .padding(.horizontal, Dimension.Description.Padding.leading)
-                                Text(self.fontInfo.lineHeight)
-                                    .font(YDSFont.body2)
-                                    .foregroundColor(YDSColor.monoItemText)
-                                    .padding(.horizontal, Dimension.Description.Padding.trailing)
-                            }
-                        }
-                    }
-                }
+        VStack(alignment: .leading, spacing: Dimension.Spacing.vstack) {
+            HStack {
+                Text(self.fontInfo.id)
+                    .font(YDSFont.caption1)
+                    .foregroundColor(YDSColor.monoItemText)
+                    .padding(.leading, Dimension.Padding.id)
+                Spacer()
             }
+            .background {
+                RoundedRectangle(cornerRadius: Dimension.Rectangle.idCornerRadius)
+                    .fill(YDSColor.monoItemBG)
+                    .frame(height: Dimension.Rectangle.idHeight)
+            }
+            .padding(.top, Dimension.Padding.hstack)
+            Text(SampleLabel.sampleLabel)
+                .font(self.fontInfo.font ?? .title2)
+                .foregroundColor(YDSColor.textPrimary)
+            HStack(spacing: Dimension.Spacing.hstack) {
+                VStack(alignment: .leading, spacing: Dimension.Spacing.textSpacing) {
+                    Group {
+                        Text("size")
+                        Text("weight")
+                        Text("lineHeight")
+                    }
+                    .font(YDSFont.subtitle3)
+                    .foregroundColor(YDSColor.textTertiary)
+                }
+                .padding(.leading, Dimension.Padding.hstack)
+                VStack(alignment: .leading, spacing: Dimension.Spacing.textSpacing) {
+                    Group {
+                        Text(self.fontInfo.size)
+                        Text(self.fontInfo.weight)
+                        Text(self.fontInfo.lineHeight)
+                    }
+                    .font(YDSFont.body2)
+                    .foregroundColor(YDSColor.monoItemText)
+                }
+                Spacer()
+            }
+            .background {
+                RoundedRectangle(cornerRadius: Dimension.Rectangle.infoCornerRadius)
+                    .fill(YDSColor.monoItemBG)
+                    .frame(height: Dimension.Rectangle.infoHeight)
+            }
+            .padding(.vertical, Dimension.Padding.hstack)
         }
-        .padding(Dimension.Spacing.listItem)
+        .padding(.horizontal, Dimension.Spacing.listItem)
     }
 }
 
 struct TypoPageView: View {
     private let fonts = YDSSwiftUITypoWrapper.fonts
-    private let colors = YDSSwiftUIColorWrapper.colors
     var body: some View {
         List {
             ForEach(fonts) { wrappedFonts in
@@ -117,4 +109,3 @@ struct TypoPageView_Previews: PreviewProvider {
     }
 }
 // swiftlint:enable nesting
-// swiftlint:enable line_length
