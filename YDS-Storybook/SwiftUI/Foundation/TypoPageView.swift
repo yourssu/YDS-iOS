bb415b6b9ce0143985eaec324c9b438c87eaa66c
Bibi-urssu <bibi.urssu@gmail.com>
2023-08-22 15:35:50 +0900

    [#202] TypoPageView

---
+++ YDS-Storybook/SwiftUI/Foundation/TypoPageView.swift
@@ -0,0 +1,20 @@
+//
+//  TypoPageView.swift
+//  YDS-Storybook
+//
+//  Created by 박지윤 on 2023/08/22.
+//
+
+import SwiftUI
+
+struct TypoPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TypographyPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        TypoPageView()
+    }
+}
