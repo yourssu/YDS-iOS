81bf11eadf58405d4bfef8618f5996a6e9cabed4
Bibi-urssu <bibi.urssu@gmail.com>
2023-08-22 15:36:42 +0900

    [#202] SwiftUIYDSTypoArray - YDSFont

---
+++ YDS-Storybook/SwiftUI/Resources/SwiftUIYDSTypoArray.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftUIYDSTypoArray.swift
+//  YDS-Storybook
+//
+//  Created by 박지윤 on 2023/08/22.
+//
+
+import SwiftUI
+import YDS_SwiftUI
+
+let swiftUIYDSFontArray = [
+    YDSFont.display1
+    YDSFont.display2
+    YDSFont.title1
+    YDSFont.title2
+    YDSFont.title3
+    YDSFont.subtitle1
+    YDSFont.subtitle2
+    YDSFont.subtitle3
+    YDSFont.body1
+    YDSFont.body2
+    YDSFont.button0
+    YDSFont.button1
+    YDSFont.button2
+    YDSFont.button3
+    YDSFont.button4
+    YDSFont.caption0
+    YDSFont.caption1
+    YDSFont.caption2
+]
