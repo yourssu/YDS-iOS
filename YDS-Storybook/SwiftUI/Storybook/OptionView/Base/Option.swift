3f34b2511e1fe9658bb686c71754c02d35fcf7be
Sanghyeon Sim <5338095@gmail.com>
2023-08-23 20:18:22 +0900

    [#199] Option enum 내부 cases 파라미터 지정

--- YDS-Storybook/SwiftUI/Storybook/OptionView/Base/Option.swift
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/Base/Option.swift
@@ -9,20 +9,23 @@ import SwiftUI
 
 enum Option: View {
     case bool(description: String?, isOn: Binding<Bool>)
-    case `enum`, int, optionalString, optionalImage
+    case `enum`(description:String?, cases:[Any], selectedIndex:Binding<Int>)
+    case int(description: String?, value: Binding<Int>)
+    case optionalString(description: String?, defaultValue: Binding<String?>)
+    case optionalImage(description:String?, images:[Image?], selectedIndex: Binding<Int>)
     
     @ViewBuilder
     var body: some View {
         switch self {
         case .bool(let description, let isOn):
             BoolOptionView(description: description, isOn: isOn)
-        case .enum:
-            EnumOptionView()
-        case .int:
-            IntOptionView()
-        case .optionalString:
+        case .enum(let description, let cases, let selectedIndex):
+            EnumOptionView(description: description, cases: cases, selectedIndex: selectedIndex)
+        case .int(let description, let value):
+            IntOptionView(description: description, value: value)
+        case .optionalString(let description, let defaultValue):
             OptionalStringOptionView()
-        case .optionalImage:
+        case .optionalImage(let description, let images, let selectedIndex):
             OptionalImageOptionView()
         }
     }
