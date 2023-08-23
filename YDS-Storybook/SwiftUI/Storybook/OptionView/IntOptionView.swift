6f1ad70138011db0b931a4c30ed3ff9630eae7c5
Sanghyeon Sim <5338095@gmail.com>
2023-08-23 20:19:12 +0900

    [#199] IntOptionView 구현

--- YDS-Storybook/SwiftUI/Storybook/OptionView/IntOptionView.swift
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/IntOptionView.swift
@@ -6,15 +6,38 @@
 //
 
 import SwiftUI
+import YDS_SwiftUI
 
 struct IntOptionView: View {
+    private let description: String?
+    @Binding private var value: Int
+    
+    init(description: String?, value: Binding<Int>) {
+        self.description = description
+        self._value = value
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            if let description = description {
+                Text(description)
+                    .font(YDSFont.subtitle2)
+            }
+            Text("Int")
+                .font(YDSFont.body2)
+            TextField("", value: $value, format: .number)
+        }
     }
 }
 
 struct IntOptionView_Previews: PreviewProvider {
     static var previews: some View {
-        IntOptionView()
+        @State var value: Int = 0
+        let description: String = "numberOfLines"
+        
+        VStack {
+            Text("\(value)")
+            IntOptionView(description: description, value: $value)
+        }
     }
 }
