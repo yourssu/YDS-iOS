1390bcab7e41a0e7edc1dc9a713f95efc9a99545
Sanghyeon Sim <5338095@gmail.com>
2023-08-23 20:19:36 +0900

    [#199] EnumOptionView 구현

--- YDS-Storybook/SwiftUI/Storybook/OptionView/EnumOptionView.swift
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/EnumOptionView.swift
@@ -6,15 +6,44 @@
 //
 
 import SwiftUI
+import YDS_SwiftUI
 
 struct EnumOptionView: View {
+    private let description: String?
+    private let cases: [Any]
+    @Binding private var selectedIndex:Int
+    
+    @State private var showingBottomSheet = false
+    
+    init(description: String?, cases: [Any], selectedIndex:Binding<Int> ) {
+        self.description = description
+        self.cases = cases
+        self._selectedIndex = selectedIndex
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            if let description = description {
+                Text(description)
+                    .font(YDSFont.subtitle2)
+            }
+            if !cases.isEmpty {
+                let mirror = Mirror(reflecting: cases[0])
+                if let enumTypeName = "\(mirror.subjectType)".split(separator: ".").first {
+                    Text(String(enumTypeName))
+                        .font(YDSFont.body2)
+                }
+            }
+            ShowPickerButton(cases: cases.map(  {String(describing: $0)}), selectedIndex: $selectedIndex)
+        }
     }
 }
 
 struct EnumOptionView_Previews: PreviewProvider {
     static var previews: some View {
-        EnumOptionView()
+        @State var index:Int = 0
+        VStack(alignment: .leading) {
+            EnumOptionView(description: "rouding", cases: ["example1","example2"], selectedIndex: $index)
+        }
     }
 }
