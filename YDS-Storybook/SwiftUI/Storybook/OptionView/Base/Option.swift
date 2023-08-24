af73a231a89e8fc7f8fc0c94c08778fabbf4b021
jayden000106 <cars0106@icloud.com>
2023-08-24 13:56:44 +0900

    [#199] OptionalStringOptionView 구현

--- YDS-Storybook/SwiftUI/Storybook/OptionView/Base/Option.swift
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/Base/Option.swift
@@ -11,7 +11,7 @@ enum Option: View {
     case bool(description: String?, isOn: Binding<Bool>)
     case `enum`(description:String?, cases:[Any], selectedIndex:Binding<Int>)
     case int(description: String?, value: Binding<Int>)
-    case optionalString(description: String?, defaultValue: Binding<String?>)
+    case optionalString(description: String?, text: Binding<String?>)
     case optionalImage(description:String?, images:[Image?], selectedIndex: Binding<Int>)
     
     @ViewBuilder
@@ -23,8 +23,8 @@ enum Option: View {
             EnumOptionView(description: description, cases: cases, selectedIndex: selectedIndex)
         case .int(let description, let value):
             IntOptionView(description: description, value: value)
-        case .optionalString(let description, let defaultValue):
-            OptionalStringOptionView()
+        case .optionalString(let description, let text):
+            OptionalStringOptionView(description: description, text: text)
         case .optionalImage(let description, let images, let selectedIndex):
             OptionalImageOptionView()
         }
--- YDS-Storybook/SwiftUI/Storybook/OptionView/OptionalStringOptionView.swift
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/OptionalStringOptionView.swift
@@ -6,15 +6,76 @@
 //
 
 import SwiftUI
+import YDS_SwiftUI
 
 struct OptionalStringOptionView: View {
+    @Binding private var text: String?
+    
+    private let description: String?
+    
+    @State private var placeholder: String
+    
+    init(description: String?, text: Binding<String?>) {
+        self.description = description
+        self._text = text
+        self.placeholder = text.wrappedValue ?? ""
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    if let description = description {
+                        Text(description)
+                            .font(YDSFont.subtitle2)
+                    }
+                    Text("Optional<String>")
+                        .font(YDSFont.body2)
+                }
+                Spacer()
+                Toggle("", isOn: Binding(
+                    get: {
+                        text != nil
+                    },
+                    set: { isOn in
+                        isOn ? (text = placeholder) : (text = nil)
+                    }
+                ))
+                .labelsHidden()
+                .tint(YDSColor.buttonPoint)
+            }
+            
+            TextField("", text: Binding(
+                get: {
+                    placeholder
+                },
+                set: { text in
+                    placeholder = text
+                    self.text = text
+                }
+            ))
+            .font(YDSFont.body1)
+            .tint(YDSColor.textPointed)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(YDSColor.inputFieldElevated)
+            )
+        }
     }
 }
 
 struct OptionalStringOptionView_Previews: PreviewProvider {
     static var previews: some View {
-        OptionalStringOptionView()
+        VStack {
+            OptionalStringOptionView(
+                description: "text",
+                text: .constant("테스트입니다")
+            )
+            OptionalStringOptionView(
+                description: "text",
+                text: .constant(nil)
+            )
+        }
     }
 }
