e6050c0d56d533468f39e3d20a8a6b4d5302708a
Sanghyeon Sim <5338095@gmail.com>
2023-09-07 00:46:49 +0900

    [#206] OptionalImageOptionView 구현하여 이미지 enum case 추가

---
+++ YDS-Storybook/SwiftUI/Storybook/OptionView/OptionalImageOptionView.swift
@@ -0,0 +1,84 @@
+//
+//  OptionalImageOptionView.swift
+//  YDS-Storybook
+//
+//  Created by 심상현 on 2023/09/04.
+//
+// swiftlint:disable line_length
+
+import SwiftUI
+import YDS_SwiftUI
+
+
+struct OptionalImageOptionView: View {
+    private enum Dimension {
+        enum Spacing {
+            static let vstack: CGFloat = 8
+            static let textSpacing: CGFloat = 4
+        }
+        
+        enum Padding {
+            static let button: CGFloat = 16
+        }
+        
+        enum Rectangle {
+            static let cornerRadius: CGFloat = 8
+        }
+    }
+    
+    @Binding private var selectedImage: SwiftUIImage?
+    
+    private let description: String?
+    private let images: [SwiftUIImage]
+    
+    @State private var placeholderIndex: Int
+    @State private var isPresentPicker = false
+    
+    init(description: String?, images: [SwiftUIImage], selectedImage: Binding<SwiftUIImage?>) {
+        self.description = description
+        self.images = images
+        self._selectedImage = selectedImage
+        self.placeholderIndex = 0
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: Dimension.Spacing.vstack) {
+            HStack {
+                VStack(alignment: .leading, spacing: Dimension.Spacing.textSpacing) {
+                    if let description = description {
+                        Text(description)
+                            .font(YDSFont.subtitle2)
+                    }
+                    Text("Optional<Image>")
+                        .font(YDSFont.body2)
+                }
+                Spacer()
+                Toggle("", isOn: Binding(get: {
+                    selectedImage != nil
+                }, set: { isOn in
+                    isOn ? (selectedImage = images[placeholderIndex]) : (selectedImage = nil)
+                }))
+                .labelsHidden()
+                .tint(YDSColor.buttonPoint)
+            }
+            
+            ShowPickerButton(cases: images.map({ $0.name }), selectedIndex: Binding(get: {
+                placeholderIndex
+            }, set: { index in
+                placeholderIndex = index
+                selectedImage = images[index]
+            }))
+            .disabled(selectedImage == nil)
+        }
+    }
+}
+
+struct OptionalImageOptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionalImageOptionView(
+            description: "image",
+            images: YDSSwiftUIImage.images,
+            selectedImage: .constant(YDSSwiftUIImage.images[0])
+        )
+    }
+}
