a227c358577754ac902eb71d11fa6c407f38922f
Sanghyeon Sim <5338095@gmail.com>
2023-09-08 19:26:08 +0900

    [#206] Storybook에서 YDS_SwiftUI로 ProfileImageView 코드 이동 및 추석 추가

--- YDS-Storybook/SwiftUI/Atom/ProfileImagePageView.swift
+++ YDS-Storybook/SwiftUI/Atom/ProfileImagePageView.swift
@@ -9,46 +9,6 @@
 import SwiftUI
 import YDS_SwiftUI
 
-fileprivate struct ProfileImageReshaper: Shape {
-    var insetRatio: CGFloat
-    var width: CGFloat
-
-    func path(in rect: CGRect) -> Path {
-        let radius = width / 2
-        let inset = radius * insetRatio
-
-        let topPoint = CGPoint(x: radius, y: 0)
-        let rightPoint = CGPoint(x: radius * 2, y: radius)
-        let bottomPoint = CGPoint(x: radius, y: radius * 2)
-        let leftPoint = CGPoint(x: 0, y: radius)
-
-        var path = Path()
-        path.move(to: topPoint)
-        path.addCurve(
-            to: rightPoint,
-            control1: CGPoint(x: radius * 2 - inset, y: 0),
-            control2: CGPoint(x: radius * 2, y: inset)
-        )
-        path.addCurve(
-            to: bottomPoint,
-            control1: CGPoint(x: radius * 2, y: radius * 2 - inset),
-            control2: CGPoint(x: radius * 2 - inset, y: radius * 2)
-        )
-        path.addCurve(
-            to: leftPoint,
-            control1: CGPoint(x: inset, y: radius * 2),
-            control2: CGPoint(x: 0, y: radius * 2 - inset)
-        )
-        path.addCurve(
-            to: topPoint,
-            control1: CGPoint(x: 0, y: inset),
-            control2: CGPoint(x: inset, y: 0)
-        )
-
-        return path
-    }
-}
-
 struct ProfileImagePageView: View {
     let title: String = "ProfileImageView"
 
@@ -56,37 +16,23 @@ struct ProfileImagePageView: View {
     @State var sizeSelectedIndex: Int = 4
     @State var image: SwiftUIImage? = YDSSwiftUIImage.images[0]
 
-    enum ProfileImageViewSize: CGFloat {
-        case extraSmall = 24
-        case small = 32
-        case medium = 48
-        case large = 72
-        case extraLarge = 96
-    }
-
     var body: some View {
         StorybookPageView(sample: {
             VStack {
                 if let image = image?.image {
-                    image
-                        .resizable()
-                        .frame(width: ProfileImageViewSize.allCases[sizeSelectedIndex].rawValue,
-                            height: ProfileImageViewSize.allCases[sizeSelectedIndex].rawValue)
-                        .clipShape(ProfileImageReshaper(insetRatio: 0.2, width: ProfileImageViewSize.allCases[sizeSelectedIndex].rawValue))
-                        .overlay(ProfileImageReshaper(insetRatio: 0.2, width: ProfileImageViewSize.allCases[sizeSelectedIndex].rawValue)
-                            .stroke(YDSColor.borderNormal, lineWidth: YDSConstant.Border.normal))
+                    YDSProfileImageView(image: image, size: YDSProfileImageView.ProfileImageViewSize.allCases[sizeSelectedIndex])
                 }
             }
         }, options: [
             Option.optionalImage(description: "image", images: YDSSwiftUIImage.images, selectedImage: $image),
-            Option.enum(description: "size", cases: ProfileImageViewSize.allCases, selectedIndex: $sizeSelectedIndex)
+            Option.enum(description: "size", cases: YDSProfileImageView.ProfileImageViewSize.allCases, selectedIndex: $sizeSelectedIndex),
         ])
         .navigationTitle(title)
     }
 }
 
-extension ProfileImagePageView.ProfileImageViewSize: CaseIterable {
-    static var allCases: [ProfileImagePageView.ProfileImageViewSize] =
+extension YDSProfileImageView.ProfileImageViewSize: CaseIterable {
+    public static var allCases: [YDSProfileImageView.ProfileImageViewSize] =
     [.extraSmall, .small, .medium, .large, .extraLarge]
 }
 
---
+++ YDS-Storybook/SwiftUI/Resources/YDSProfileImageView.swift
@@ -0,0 +1,122 @@
+//
+//  YDSProfileImageView.swift
+//  YDS-SwiftUI
+//
+//  Created by 심상현 on 2023/09/08.
+//
+
+import SwiftUI
+import YDS_Essential
+
+/// 이미지를 ProfileImage 형태로 변환하는 Shape
+private struct ProfileImageReshaper: Shape {
+    var insetRatio: CGFloat // 내부 여백 비율
+    var width: CGFloat // 이미지 너비
+
+    func path(in rect: CGRect) -> Path {
+        let radius = width / 2
+        let inset = radius * insetRatio
+
+        let topPoint = CGPoint(x: radius, y: 0)
+        let rightPoint = CGPoint(x: radius * 2, y: radius)
+        let bottomPoint = CGPoint(x: radius, y: radius * 2)
+        let leftPoint = CGPoint(x: 0, y: radius)
+
+        var path = Path()
+        path.move(to: topPoint)
+        path.addCurve(
+            to: rightPoint,
+            control1: CGPoint(x: radius * 2 - inset, y: 0),
+            control2: CGPoint(x: radius * 2, y: inset)
+        )
+        path.addCurve(
+            to: bottomPoint,
+            control1: CGPoint(x: radius * 2, y: radius * 2 - inset),
+            control2: CGPoint(x: radius * 2 - inset, y: radius * 2)
+        )
+        path.addCurve(
+            to: leftPoint,
+            control1: CGPoint(x: inset, y: radius * 2),
+            control2: CGPoint(x: 0, y: radius * 2 - inset)
+        )
+        path.addCurve(
+            to: topPoint,
+            control1: CGPoint(x: 0, y: inset),
+            control2: CGPoint(x: inset, y: 0)
+        )
+
+        return path
+    }
+}
+
+public struct YDSProfileImageView: View {
+    /// 프로필 이미지 크기
+    public enum ProfileImageViewSize: CGFloat {
+        case extraSmall = 24
+        case small = 32
+        case medium = 48
+        case large = 72
+        case extraLarge = 96
+    }
+
+    private let image: Image?
+    private let size: ProfileImageViewSize
+    private var color = YDSColor.borderNormal
+    private var width = YDSConstant.Border.normal
+
+    /**
+     프로필 이미지와 크기를 설정하는 초기화 함수
+     
+     - Parameters:
+        - image: 표시할 이미지
+        - size: 이미지의 크기. `ProfileImageViewSize` enum 값을 사용
+     
+     - Returns: YDSProfileImageView 인스턴스
+     */
+    public init(image: Image, size: ProfileImageViewSize?) {
+        self.image = image
+        self.size = size ?? .small
+    }
+
+    /**
+     테두리 색상과 두께를 설정
+     
+     - Parameters:
+        - color: 테두리 색상
+        - width: 테두리 두께
+     
+     - Returns: 테두리가 설정된 YDSProfileImageView 인스턴스
+     */
+    public func border(color: Color, width: CGFloat) -> YDSProfileImageView {
+        var view = self
+        view.color = color
+        view.width = width
+        return view
+    }
+
+    /**
+     초기화 실패 시 호출되는 이니셜라이저
+     
+     - Returns: nil
+     */
+    init?() {
+        fatalError("initialize")
+    }
+
+    public var body: some View {
+        if let image = image {
+            image
+                .resizable()
+                .frame(width: size.rawValue, height: size.rawValue)
+                .clipShape(ProfileImageReshaper(insetRatio: 0.2, width: size.rawValue))
+                .overlay(ProfileImageReshaper(insetRatio: 0.2, width: size.rawValue)
+                    .stroke(color, lineWidth: width))
+        }
+    }
+}
+
+struct YDSProfileImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        YDSProfileImageView(image: Image(""), size: YDSProfileImageView.ProfileImageViewSize.large)
+    }
+}
