e081ed81f8328c612149c752235c376564898516
Bibi-urssu <bibi.urssu@gmail.com>
2023-09-15 21:35:47 +0900

    [#211-2] LabelPageView 수정 사항 반영

--- YDS-Storybook/SwiftUI/Atom/LabelPageView.swift
+++ YDS-Storybook/SwiftUI/Atom/LabelPageView.swift
@@ -7,28 +7,73 @@
 import SwiftUI
 import YDS_SwiftUI
 
+struct UIKitLabelView: UIViewRepresentable {
+    let text: String
+    let typoStyle: UIFont
+    let textColor: UIColor?
+
+    var maxWidth: CGFloat
+
+    let numberOfLines: Int
+    let lineBreakMode: NSLineBreakMode
+    let alignment: NSTextAlignment
+    let lineBreakStrategy: NSParagraphStyle.LineBreakStrategy
+
+    func makeUIView(context: Context) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = typoStyle
+        label.textColor = textColor
+        label.numberOfLines = numberOfLines
+        label.lineBreakMode = lineBreakMode
+        label.textAlignment = alignment
+        label.lineBreakStrategy = lineBreakStrategy
+        label.preferredMaxLayoutWidth = maxWidth
+
+        return label
+    }
+
+    func updateUIView(_ uiView: UILabel, context: Context) {
+        uiView.text = text
+        uiView.font = typoStyle
+        uiView.textColor = textColor
+        uiView.numberOfLines = numberOfLines
+        uiView.lineBreakMode = lineBreakMode
+        uiView.textAlignment = alignment
+        uiView.lineBreakStrategy = lineBreakStrategy
+    }
+}
+
 struct LabelPageView: View {
     let title: String = "Label"
 
-    @State var typoStyleSelectedIndex: Int = 0
+    @State var typoStyleSelectedIndex = 0
     @State var textColorSelectedIndex: Int = 0
-    @State var truncationModeSelectedIndex: Int = 0
+    @State var lineBreakModeSelectedIndex: Int = 0
+    @State var alignmentSelectedIndex: Int = 1
+    @State var lineBreakStrategySelectedIndex: Int = 2
 
     @State var text: String? = "Label"
     @State var int: Int = 0
-    @State var textColor: WrappedSwiftUIColor? = YDSSwiftUIColorWrapper.textColors.items[0]
 
     var body: some View {
         StorybookPageView(sample: {
             VStack {
-                textColor?.color.map {
-                    YDSLabelView(
-                        text: Text(text ?? "")
-                            .font(Font(String.TypoStyle.allCases[typoStyleSelectedIndex].font))
-                            .foregroundColor($0)
+                GeometryReader { geometry in
+                    let UITextColor = UIColor(YDSSwiftUIColorWrapper.textColors.items[textColorSelectedIndex].color ?? .black)
+                    UIKitLabelView(
+                        text: text ?? "",
+                        typoStyle: String.TypoStyle.allCases[typoStyleSelectedIndex].font,
+                        textColor: UITextColor,
+                        maxWidth: geometry.size.width - 32,
+                        numberOfLines: int,
+                        lineBreakMode: NSLineBreakMode.allCases[lineBreakModeSelectedIndex],
+                        alignment: NSTextAlignment.allCases[alignmentSelectedIndex],
+                        lineBreakStrategy: NSParagraphStyle.LineBreakStrategy.allCases[lineBreakStrategySelectedIndex]
                     )
-                    .lineLimit(int == 0 ? nil : int)
-                    .truncationMode(Text.TruncationMode.allCases[truncationModeSelectedIndex])
+                    .frame(height: YDSScreenSize.width * 3/4 - 32)
+                    .padding(16)
+                    .clipped()
                 }
             }
         }, options: [
@@ -40,23 +85,25 @@ struct LabelPageView: View {
                 cases: String.TypoStyle.allCases,
                 selectedIndex: $typoStyleSelectedIndex),
             Option.int(description: "numberOfLines", value: $int),
-            Option.optionalTextColor(
-                description: "textColor",
-                textColor: YDSSwiftUIColorWrapper.textColors.items,
-                selectedTextColor: $textColor),
             Option.enum(
-                description: "truncationMode",
-                cases: Text.TruncationMode.allCases,
-                selectedIndex: $truncationModeSelectedIndex)
+                description: "textColor",
+                cases: YDSSwiftUIColorWrapper.textColors.items,
+                selectedIndex: $textColorSelectedIndex),
+            Option.enum(
+                description: "lineBreakMode",
+                cases: NSLineBreakMode.allCases,
+                selectedIndex: $lineBreakModeSelectedIndex),
+            Option.enum(
+                description: "alignment",
+                cases: NSTextAlignment.allCases,
+                selectedIndex: $alignmentSelectedIndex),
+            Option.enum(
+                description: "lineBreakStrategy",
+                cases: NSParagraphStyle.LineBreakStrategy.allCases,
+                selectedIndex: $lineBreakStrategySelectedIndex)
         ])
         .navigationTitle(title)
     }
-}
-
-extension Text.TruncationMode: CaseIterable {
-    public static var allCases: [Text.TruncationMode] = [.head,
-                                                           .tail,
-                                                           .middle]
 }
 
 struct LabelPageView_Previews: PreviewProvider {
