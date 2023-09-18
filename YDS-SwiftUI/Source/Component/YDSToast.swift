f6328fd52f58df5759d629d65f00d6cb506cf0d3
Sanghyeon Sim <5338095@gmail.com>
2023-09-18 23:06:41 +0900

    [#215] Component 그룹내 파일용 Toast 추가

---
+++ YDS-SwiftUI/Source/Component/YDSToast.swift
@@ -0,0 +1,20 @@
+//
+//  YDSToast.swift
+//  YDS-SwiftUI
+//
+//  Created by 심상현 on 2023/09/18.
+//
+
+import SwiftUI
+
+struct YDSToast: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct YDSToast_Previews: PreviewProvider {
+    static var previews: some View {
+        YDSToast()
+    }
+}
