14031b16be68aecc5ac8d0e563efe795069ebe71
jayden000106 <cars0106@icloud.com>
2023-10-31 02:37:54 +0900

    [#226] 필요 파일 추가

---
+++ YDS-Storybook/SwiftUI/Atom/BottomSheetPageView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSheetPageView.swift
+//  YDS-Storybook
+//
+//  Created by 정지혁 on 2023/10/31.
+//
+
+import SwiftUI
+
+struct BottomSheetPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomSheetPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomSheetPageView()
+    }
+}
