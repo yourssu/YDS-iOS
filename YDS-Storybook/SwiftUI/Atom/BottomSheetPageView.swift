ef1a7b75ec9debab29823fcc88431ec41b21424a
jayden000106 <cars0106@icloud.com>
2023-10-31 19:36:56 +0900

    [#226] BottomSheetPageView 구현

--- YDS-Storybook/SwiftUI/Atom/BottomSheetPageView.swift
+++ YDS-Storybook/SwiftUI/Atom/BottomSheetPageView.swift
@@ -6,10 +6,39 @@
 //
 
 import SwiftUI
+import YDS_SwiftUI
 
 struct BottomSheetPageView: View {
+    private let views = ["zero", "one", "two", "three", "four"]
+
+    @State var selectedIndex = 0
+    @State var isPresentBottomSheet = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            StorybookPageView(
+                sample: {
+                    Rectangle()
+                        .fill(Color.clear)
+                        .frame(height: YDSScreenSize.width * 3/4 - 32)
+                },
+                options: [
+                    Option.enum(
+                        description: "Views",
+                        cases: views,
+                        selectedIndex: $selectedIndex
+                    )
+                ]
+            )
+            .overlay(alignment: .bottom) {
+                // TODO: 컴포넌트 버튼으로 수정
+                Button(action: {
+                    isPresentBottomSheet = true
+                }, label: {
+                    Text("바텀시트 생성")
+                })
+                .padding(.bottom, 32)
+            }
+            .ydsBottomSheet(isPresent: $isPresentBottomSheet)
     }
 }
 
