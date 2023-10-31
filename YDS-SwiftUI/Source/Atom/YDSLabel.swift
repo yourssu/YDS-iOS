94ad6b9dbbc4fa1c1805fbb57483255c518a5af0
chongin12 <chongin12@naver.com>
2023-11-01 01:35:03 +0900

    [#223] YDSLabel typoStyle 따로 case 구현

--- YDS-SwiftUI/Source/Atom/YDSLabel.swift
+++ YDS-SwiftUI/Source/Atom/YDSLabel.swift
@@ -18,8 +18,75 @@ public struct YDSLabel: UIViewRepresentable {
     let alignment: NSTextAlignment?
     let lineBreakStrategy: NSParagraphStyle.LineBreakStrategy?
 
+    public enum TypoStyle: CaseIterable {
+        case display1
+        case display2
+
+        case title1
+        case title2
+        case title3
+
+        case subtitle1
+        case subtitle2
+        case subtitle3
+
+        case body1
+        case body2
+
+        case button0
+        case button1
+        case button2
+        case button3
+        case button4
+
+        case caption0
+        case caption1
+        case caption2
+
+        var uiFont: UIFont {
+            switch self {
+            case .display1:
+                return UIFont.systemFont(ofSize: 40, weight: .bold )
+            case .display2:
+                return UIFont.systemFont(ofSize: 32, weight: .bold )
+            case .title1:
+                return UIFont.systemFont(ofSize: 28, weight: .bold )
+            case .title2:
+                return UIFont.systemFont(ofSize: 24, weight: .bold )
+            case .title3:
+                return UIFont.systemFont(ofSize: 20, weight: .bold )
+            case .subtitle1:
+                return UIFont.systemFont(ofSize: 20, weight: .semibold )
+            case .subtitle2:
+                return UIFont.systemFont(ofSize: 16, weight: .semibold )
+            case .subtitle3:
+                return UIFont.systemFont(ofSize: 14, weight: .semibold )
+            case .body1:
+                return UIFont.systemFont(ofSize: 15, weight: .regular )
+            case .body2:
+                return UIFont.systemFont(ofSize: 14, weight: .regular )
+            case .button0:
+                return UIFont.systemFont(ofSize: 16, weight: .medium )
+            case .button1:
+                return UIFont.systemFont(ofSize: 16, weight: .semibold )
+            case .button2:
+                return UIFont.systemFont(ofSize: 14, weight: .semibold )
+            case .button3:
+                return UIFont.systemFont(ofSize: 14, weight: .regular )
+            case .button4:
+                return UIFont.systemFont(ofSize: 12, weight: .medium )
+            case .caption0:
+                return UIFont.systemFont(ofSize: 12, weight: .semibold )
+            case .caption1:
+                return UIFont.systemFont(ofSize: 12, weight: .regular )
+            case .caption2:
+                return UIFont.systemFont(ofSize: 11, weight: .regular )
+            }
+        }
+    }
+
     public init(text: String,
-                typoStyle: UIFont = UIFont.systemFont(ofSize: 17),
+                typoStyle: TypoStyle = .body1,
                 textColor: Color = Color.black,
                 maxWidth: CGFloat = .greatestFiniteMagnitude,
                 numberOfLines: Int = 0,
@@ -27,7 +94,7 @@ public struct YDSLabel: UIViewRepresentable {
                 alignment: NSTextAlignment = .center,
                 lineBreakStrategy: NSParagraphStyle.LineBreakStrategy = .hangulWordPriority) {
         self.text = text
-        self.typoStyle = typoStyle
+        self.typoStyle = typoStyle.uiFont
         self.textColor = textColor
         self.maxWidth = maxWidth
         self.numberOfLines = numberOfLines
