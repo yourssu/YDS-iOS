28998bcb52cb6405a6fcdc3538a7bcc944ee94b4
jayden000106 <cars0106@icloud.com>
2023-10-31 19:36:56 +0900

    [#226] BottomSheetPageView 구현

--- YDS-SwiftUI/Source/Atom/YDSBottomSheet.swift
+++ YDS-SwiftUI/Source/Atom/YDSBottomSheet.swift
@@ -6,3 +6,26 @@
 //
 
 import SwiftUI
+
+struct YDSBottomSheet: ViewModifier {
+    @State var height = CGFloat.zero
+
+    @Binding var isPresent: Bool
+
+    func body(content: Content) -> some View {
+        content
+            .sheet(isPresented: $isPresent) {
+                Text("Hello")
+                    .presentationDetents([.medium])
+            }
+    }
+}
+
+extension View {
+    public func ydsBottomSheet(
+        isPresent: Binding<Bool>
+    ) -> some View {
+        self
+            .modifier(YDSBottomSheet(isPresent: isPresent))
+    }
+}
