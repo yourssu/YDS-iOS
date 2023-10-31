9e4ce633c093ac3235a6ecb7fd9a5d1d80104720
JJongW <shinjw4675@gmail.com>
2023-10-31 11:14:44 +0900

    [#219] YDSTopBarView

---
+++ YDS-SwiftUI/Source/Component/YDSTopBarEx.swift
@@ -0,0 +1,61 @@
+//
+//  YDSTopBarEx.swift
+//  YDS-SwiftUI
+//
+//  Created by 신종원 on 2023/10/30.
+//
+
+import Foundation
+import SwiftUI
+
+public struct P263_NavigationBar: View {
+    
+    @State private var centerText = ""
+    @State private var showLeftAlert: Bool = false
+    @State private var showRightAlert: Bool = false
+    
+    public init() {}
+    public var body: some View {
+    #if os(iOS)
+        NavigationView {
+            Text(centerText)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Left") {
+                            centerText = "Left Button Clicked"
+                            showLeftAlert = true
+                        }.alert(isPresented: $showLeftAlert) {
+                            Alert(title: Text("Left"), message: Text("Button Clicked"), dismissButton: .default(Text("Dismiss")))
+                        }
+                    }
+                    ToolbarItem(placement: .principal) {
+                        VStack{
+                            Text("Title")
+                                .font(.system(size: 20, weight: .bold))
+                            Text("SubTitle")
+                                .font(.system(size: 10))
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Right") {
+                            centerText = "Right Button Clicked"
+                            showRightAlert = true
+                        }.alert(isPresented: $showRightAlert) {
+                            Alert(title: Text("Right"), message: Text("Button Clicked"), dismissButton: .default(Text("Dismiss")))
+                        
+                        }
+                    }
+                }
+        }
+    #else
+        EmptyView()
+    #endif
+    }
+}
+
+struct P263_NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        P263_NavigationBar()
+    }
+}
