0b0a4e3cc57bf31e2fa7fdc93f3323c946e5bc89
jayden000106 <cars0106@icloud.com>
2023-11-02 11:24:28 +0900

    [#226] YDSBottomSheet 및 BottomSheetPageView 구현
    - DragGesture, Transition을 이용해서 Custom Bottom Sheet 구현

--- YDS-Storybook/SwiftUI/Atom/BottomSheetPageView.swift
+++ YDS-Storybook/SwiftUI/Atom/BottomSheetPageView.swift
@@ -9,10 +9,30 @@ import SwiftUI
 import YDS_SwiftUI
 
 struct BottomSheetPageView: View {
-    private let views = ["zero", "one", "two", "three", "four"]
+    private enum YourssuDevTeam: String, CaseIterable {
+        case iOS = "iOS"
+        case android = "Android"
+        case backend = "Backend"
+        case frontend = "Frontend"
+    }
+
+    private enum ExampleCase: Int, CaseIterable {
+        case zero = 0
+        case one = 1
+        case two = 2
+        case three = 3
+        case four = 4
+    }
+
+    private let allViews = ExampleCase.allCases
+    private let allTeams = YourssuDevTeam.allCases
 
     @State var selectedIndex = 0
     @State var isPresentBottomSheet = false
+
+    private var teams: [String] {
+        allTeams[0..<selectedIndex].map { $0.rawValue }
+    }
 
     var body: some View {
             StorybookPageView(
@@ -24,21 +44,31 @@ struct BottomSheetPageView: View {
                 options: [
                     Option.enum(
                         description: "Views",
-                        cases: views,
+                        cases: allViews,
                         selectedIndex: $selectedIndex
                     )
                 ]
             )
             .overlay(alignment: .bottom) {
-                // TODO: 컴포넌트 버튼으로 수정
-                Button(action: {
+                Button(action: { // TODO: 컴포넌트 버튼으로 수정
                     isPresentBottomSheet = true
                 }, label: {
                     Text("바텀시트 생성")
                 })
                 .padding(.bottom, 32)
             }
-            .ydsBottomSheet(isPresent: $isPresentBottomSheet)
+            .ydsBottomSheet(
+                isPresent: $isPresentBottomSheet,
+                content: {
+                    VStack(alignment: .leading) {
+                        ForEach(teams, id: \.self) { team in
+                            YDSLabel(text: team, typoStyle: String.TypoStyle.title1.font)
+                                .frame(height: 36) // TODO: 이슈 해결 후 수정
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            )
     }
 }
 
