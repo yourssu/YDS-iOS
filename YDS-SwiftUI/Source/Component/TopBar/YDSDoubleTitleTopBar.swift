54e927fcdfd4718ac443c65348ca52862679a524
JJongW <shinjw4675@gmail.com>
2024-01-08 20:24:01 +0900

    [#219] TopBar 리뷰 반영 및 SwiftLint 수정

--- YDS-SwiftUI/Source/Component/TopBar/YDSDoubleTitleTopBar.swift
+++ YDS-SwiftUI/Source/Component/TopBar/YDSDoubleTitleTopBar.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct YDSDoubleTitleTopBarModifier: ViewModifier {
     @Binding public var topBar: YDSDoubleTitleTopBar
     @Binding var isShowing: Bool
-    
+
     public func body(content: Content) -> some View {
         return content
             .toolbar {
@@ -26,7 +26,6 @@ struct YDSDoubleTitleTopBarModifier: ViewModifier {
                                 YDSIcon.plusLine
                             })
                         }
-                
                 ToolbarItem(placement:
                         .topBarTrailing) {
                             Button(action: {
@@ -55,8 +54,8 @@ public struct YDSDoubleTitleTopBar: Equatable {
 struct DoubleTitleBar: View {
     public var title: String = "시간표(2안)"
     public var subtitle: String = "2021년 2학기"
-    
-    public var body : some View {
+
+    public var body: some View {
         VStack(alignment: .leading) {
             Text(subtitle)
                 .font(YDSFont.body2)
@@ -67,18 +66,21 @@ struct DoubleTitleBar: View {
 }
 
 extension View {
-    public func ydsDoubleTitleTopBar(title: Binding<String?>, subtitle: Binding<String?>, isShowing: Binding<Bool>) -> some View {
-      modifier(
-        YDSDoubleTitleTopBarModifier( 
-            topBar: .init(get: {
-                YDSDoubleTitleTopBar(
-                    title: title.wrappedValue ?? "",
-                    subtitle: subtitle.wrappedValue ?? "")
-            }, set: { ydsTopBar in
-                title.wrappedValue = ydsTopBar.title
-                subtitle.wrappedValue = ydsTopBar.subtitle
-            }), isShowing: isShowing
-        )
-      )
-  }
+    public func ydsDoubleTitleTopBar(
+        title: Binding<String?>,
+        subtitle: Binding<String?>,
+        isShowing: Binding<Bool>) -> some View {
+            modifier(
+                YDSDoubleTitleTopBarModifier(
+                    topBar: .init(get: {
+                        YDSDoubleTitleTopBar(
+                            title: title.wrappedValue ?? "",
+                            subtitle: subtitle.wrappedValue ?? "")
+                    }, set: { ydsTopBar in
+                        title.wrappedValue = ydsTopBar.title
+                        subtitle.wrappedValue = ydsTopBar.subtitle
+                    }), isShowing: isShowing
+                )
+            )
+        }
 }
