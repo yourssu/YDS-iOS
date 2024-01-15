82a091bdf839e3998b0df992060ee77b66d29b4f
Bibi-urssu <bibi.urssu@gmail.com>
2024-01-16 03:35:54 +0900

    [#250] YDSLabel & LabelPageView

--- YDS-SwiftUI/Source/Foundation/YDSTypoStyle.swift
+++ YDS-SwiftUI/Source/Foundation/YDSTypoStyle.swift
@@ -103,5 +103,12 @@ extension String {
                 return 1.3
             }
         }
+
+        public static var allCases: [String.TypoStyle] = [.display1, .display2,
+                                                          .title1, .title2, .title3,
+                                                          .subtitle1, .subtitle2, .subtitle3,
+                                                          .body1, .body2,
+                                                          .button0, .button1, .button2, .button3, .button4,
+                                                          .caption0, .caption1, .caption2]
     }
 }
